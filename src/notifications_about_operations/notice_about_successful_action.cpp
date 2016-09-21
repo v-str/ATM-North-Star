4c2cf823603aaebc96fe630661208734c170591e
Slavin209 <wealthmydestination@gmail.com>
2016-09-21 23:11:10 +0300

    Method NoticeAboutSuccessfulAction was refactored.

--- src/notifications_about_operations/notice_about_successful_action.cpp
+++ src/notifications_about_operations/notice_about_successful_action.cpp
@@ -4,9 +4,13 @@ void NoticeAboutSuccessfulAction::NoticeAboutSuccessfulRegistration(
     CashOperator &cash_operator) const {
   utility_.IgnoreCinLine();
   utility_.ClearScreen();
-  cout << "\n\n\t\t------------------\n";
+  cout << "\n\n\t\t";
+  utility_.WriteSymbolsNTimes('-', 18);
+  cout << "\n";
   utility_.WriteTextWithDelay("\t\t| Access allowed |");
-  cout << "\t\t------------------\n";
+  cout << "\t\t";
+  utility_.WriteSymbolsNTimes('-', 17);
+  cout << "\t\t\n";
   utility_.Sleep(1000);
   cash_operator.SetCredit(0);
   cash_operator.SetMonthlyPayment(0.0);
