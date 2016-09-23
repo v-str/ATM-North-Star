931e0ca63448d3a7f58919d8449565d69208dfb6
Slavin209 <wealthmydestination@gmail.com>
2016-09-23 18:05:09 +0300

    Class was refactored. All digits were changed on const.

--- src/atm_operations/refill/refill.cpp
+++ src/atm_operations/refill/refill.cpp
@@ -1,10 +1,14 @@
 #include "refill.h"
 
+static const int kint_null = 0;
+static const int minimal_refill = 10;
+static const int maximal_refill = 50000;
+
 bool Refill::StartRefillOperation(CashOperator &cash_operator) {
   refill_messenger_.ShowNotifyAboutCash();
-  int money = 0;
+  int money = kint_null;
   cin >> money;
-  if (money >= 10 && money <= 50000) {
+  if (money >= minimal_refill && money <= maximal_refill) {
     cash_operator.AddCash(money);
     refill_messenger_.ShowUserBalance(cash_operator.GetCash());
   } else {
