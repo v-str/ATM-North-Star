c1fab11ab4fb6ea675878ed1f392abe9845fabfa
Slevin <wealthmydestination@gmail.com>
2016-11-01 12:34:00 +0300

    New line symbol was added.

--- src/atm_operations/refill/refill_messenger.cpp
+++ src/atm_operations/refill/refill_messenger.cpp
@@ -7,7 +7,7 @@ void RefillMessenger::ShowNotifyAboutCash() const {
   utility_.WriteTextWithDelay(
       "\t Notify:\n\t The sum must be more than\n"
       "\t 10$ and less than 50000$");
-  std::cout << "\t";
+  std::cout << "\n\t";
   utility_.WriteSymbolsNTimes('-', kFrame);
   std::cout << "\n";
   std::cout << "\t# Please enter the sum of money($): ";
