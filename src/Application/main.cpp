e9e41d812ab9b07470d18a370b6f2935d4ad25fe
Slevin <wealthmydestination@gmail.com>
2016-11-20 23:29:22 +0300

    The first version of output in file.

--- src/Application/main.cpp
+++ src/Application/main.cpp
@@ -1,9 +1,17 @@
+#include <text_writer.h>
 #include "application.h"
 
-int main() {
-  Application app;
+#include <fstream>
+#include <iostream>
 
+int main() {
+  std::ofstream stream_output;
+  stream_output.open("Atm_file.txt");
+  stream_output << "Atm file:" << std::endl << std::endl;
+
+  TextWriter::SetOutputStream(&stream_output);
+  Application app;
   app.RunProgram();
 
-
+  stream_output.close();
 }
--- src/Messengers/text_writer.cpp
+++ src/Messengers/text_writer.cpp
@@ -5,7 +5,10 @@
 
 std::ostream *TextWriter::output_stream_ = &std::cout;
 
-void TextWriter::Write(const std::string &text) { *output_stream_ << text; }
+void TextWriter::Write(const std::string &text) {
+  *output_stream_ << text;
+  output_stream_->flush();
+}
 
 void TextWriter::SetOutputStream(std::ostream *output_stream) {
   output_stream_ = output_stream;
