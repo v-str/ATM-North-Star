70d52ca5512e6ffeb722fd1b45566caea8f9fc2c
Slevin <wealthmydestination@gmail.com>
2016-12-07 18:36:42 +0300

    Method ShowNotifyAboutCash was refactored.

--- src/Messengers/refill_messenger.cpp
+++ src/Messengers/refill_messenger.cpp
@@ -3,21 +3,25 @@
 #include <iostream>
 
 void RefillMessenger::ShowNotifyAboutCash() const {
-  std::cout << "\t";
+  // std::cout << "\t";
+  console_editor_.WriteText("\t");
   console_editor_.WriteSymbolsNTimes('-', kFrame);
-  std::cout << "\n\t";
+  //  std::cout << "\n\t";
+  console_editor_.WriteText("\n\t");
   console_editor_.WriteTextWithDelayPerSymbol(
       "\t Notify:\n\t The sum must be more than\n"
       "\t 10$ and less than 50000$\n\t");
   console_editor_.WriteSymbolsNTimes('-', kFrame);
-  std::cout << "\n";
-  std::cout << "\t# Please enter the sum of money($): ";
+  //  std::cout << "\n";
+  //  std::cout << "\t# Please enter the sum of money($): ";
+  console_editor_.WriteText("\n\t# Please enter the sum of money($): ");
 }
 
 void RefillMessenger::ShowUserBalance(const int balance) const {
   std::cout << "\n\t";
   console_editor_.WriteSymbolsNTimes('-', kFrame);
-  console_editor_.WriteTextWithDelayPerSymbol("\n\t# Balance refill completed successfully.");
+  console_editor_.WriteTextWithDelayPerSymbol(
+      "\n\t# Balance refill completed successfully.");
   WriteUserInfo("Balance", std::to_string(balance));
   std::cout << "\n\t";
   console_editor_.Sleep(kSecond);
