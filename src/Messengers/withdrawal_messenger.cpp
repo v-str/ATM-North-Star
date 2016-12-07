d8dd883c8595f18442d1278c6ff23cefa43fca7a
Slevin <wealthmydestination@gmail.com>
2016-12-07 20:26:21 +0300

    withdrawal_messenger.cpp was refactored.

--- src/Messengers/withdrawal_messenger.cpp
+++ src/Messengers/withdrawal_messenger.cpp
@@ -1,27 +1,32 @@
 #include "withdrawal_messenger.h"
 
-#include <iostream>
-
 void WithdrawalMessenger::ShowSumOfWithdrawal(int sum_of_cash) const {
-  std::cout << "\t#Sum($): " << sum_of_cash << "\n";
+  console_editor_.WriteTextWithDelayPerSymbol(
+      "\t# Sum($): " + std::to_string(sum_of_cash) + "\n");
 }
 
 void WithdrawalMessenger::ShowPasswordString() const {
-  std::cout << "\t# Please enter your password: XXXX\b\b\b\b";
+  console_editor_.WriteTextWithDelayPerSymbol(
+      "\t# Please enter your password: XXXX");
+
+  console_editor_.WriteTextWithDelayPerSymbol("\b\b\b\b", 50);
 }
 
 void WithdrawalMessenger::ShowIncorrectPasswordMessage() const {
   console_editor_.ClearScreen();
-  console_editor_.WriteTextWithDelayPerSymbol("\n\t# Sorry, entered password is incorrect.\n");
+  console_editor_.WriteTextWithDelayPerSymbol(
+      "\n\t# Sorry, entered password is incorrect.\n");
 }
 
 void WithdrawalMessenger::EnterSumOfWithdrawal() const {
-  std::cout << "\n\t# Please, enter the required sum: ";
+  console_editor_.WriteTextWithDelayPerSymbol(
+      "\n\t# Please, enter the required sum: ");
 }
 
 void WithdrawalMessenger::ShowSuccessfulWithdrawal(int sum_of_cash,
                                                    int sum_of_withdrawal) {
-  console_editor_.WriteTextWithDelayPerSymbol("\n\t# Withdrawal completed successfully\n");
-  std::cout << "\t# Sum($): " << sum_of_cash << "\n";
-  std::cout << "\t# Balance($): " << sum_of_withdrawal << "\n\n";
+  console_editor_.WriteTextWithDelayPerSymbol(
+      "\n\t# Withdrawal completed successfully\n\t# Sum($): " +
+      std::to_string(sum_of_cash) + "\n\t# Balance($): " +
+      std::to_string(sum_of_withdrawal) + "\n\n");
 }
