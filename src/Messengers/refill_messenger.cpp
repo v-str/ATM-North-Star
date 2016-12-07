86a0f4383535e8579ccdedd5009b81bd866a801f
Slevin <wealthmydestination@gmail.com>
2016-12-07 18:42:59 +0300

    Method ShowUserBalance was refactored.

--- src/Messengers/refill_messenger.cpp
+++ src/Messengers/refill_messenger.cpp
@@ -3,27 +3,23 @@
 #include <iostream>
 
 void RefillMessenger::ShowNotifyAboutCash() const {
-  // std::cout << "\t";
   console_editor_.WriteText("\t");
   console_editor_.WriteSymbolsNTimes('-', kFrame);
-  //  std::cout << "\n\t";
   console_editor_.WriteText("\n\t");
   console_editor_.WriteTextWithDelayPerSymbol(
       "\t Notify:\n\t The sum must be more than\n"
       "\t 10$ and less than 50000$\n\t");
   console_editor_.WriteSymbolsNTimes('-', kFrame);
-  //  std::cout << "\n";
-  //  std::cout << "\t# Please enter the sum of money($): ";
   console_editor_.WriteText("\n\t# Please enter the sum of money($): ");
 }
 
 void RefillMessenger::ShowUserBalance(const int balance) const {
-  std::cout << "\n\t";
+  console_editor_.WriteText("\n\t");
   console_editor_.WriteSymbolsNTimes('-', kFrame);
   console_editor_.WriteTextWithDelayPerSymbol(
-      "\n\t# Balance refill completed successfully.");
+      "\n\t# Balance refill completed successfully.\n");
   WriteUserInfo("Balance", std::to_string(balance));
-  std::cout << "\n\t";
+  console_editor_.WriteText("\n\t");
   console_editor_.Sleep(kSecond);
 }
 
