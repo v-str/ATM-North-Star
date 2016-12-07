c41b4d9bd087fb7174bdde8477a1da4bae569779
Slevin <wealthmydestination@gmail.com>
2016-12-07 16:08:31 +0300

    initial_messenger.cpp was refactored.

--- src/Messengers/initial_messenger.cpp
+++ src/Messengers/initial_messenger.cpp
@@ -1,12 +1,5 @@
 #include "initial_messenger.h"
 
-#include <iostream>
-#include <string>
-
-using std::string;
-using std::cout;
-using std::cin;
-
 void InitialMessenger::DisplayInitialScreen() {
   DisplayLogotype();
   DisplayInitialMenu();
@@ -37,19 +30,19 @@ void InitialMessenger::DisplayLogotype() {
   console_editor_.WriteTextWithDelayPerSymbol(
       "\t         ATM #0001\n"
       "      28 Greene St, New York, NY 10012\n");
-  console_editor_.WriteTextWithDelayPerSymbol("\t      press \"Enter\"\n", 50);
+  console_editor_.WriteTextWithDelayPerSymbol("\t      press \"Enter\"\n");
   console_editor_.IgnoreCinLine();
-  system("pause");
+  console_editor_.Pause();
   console_editor_.ClearScreen();
 }
 
 void InitialMessenger::DisplayInitialMenu() const {
-  cout << "\n\n\n\n\t ##################\n"
-          "\t #  1. Demo mode  #\n"
-          "\t #  2. Sign-in    #\n"
-          "\t ##################\n"
-          "\t   Enter: ___"
-       << "\b\b";
+  console_editor_.WriteText(
+      "\n\n\n\n\t ##################\n"
+      "\t #  1. Demo mode  #\n"
+      "\t #  2. Sign-in    #\n"
+      "\t ##################\n"
+      "\t   Enter: ___\b\b");
 }
 
 void InitialMessenger::DisplayError() const {
