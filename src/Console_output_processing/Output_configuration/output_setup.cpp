dde19efe607ddd8afbc44474e303cba087332891
Slevin Read <1.ordinarymind@gmail.com>
2016-12-13 14:48:08 +0300

    Responsibilities in main static method of setting the streams were parsed.

--- src/Console_output_processing/Output_configuration/output_setup.cpp
+++ src/Console_output_processing/Output_configuration/output_setup.cpp
@@ -1,5 +1,8 @@
 #include "output_setup.h"
 
+#include "console_writer.h"
+#include "text_writer.h"
+
 std::string OutputSetup::name_of_file_ = "ATM.txt";
 
 void OutputSetup::SetupDefaultStream() {
@@ -11,14 +14,7 @@ void OutputSetup::SetupTwoStreamsForOutput() {
   std::unique_ptr<ConsoleWriter> console_writer(new ConsoleWriter);
   std::unique_ptr<FileWriter> file_writer(new FileWriter(name_of_file_));
 
-  std::vector<std::unique_ptr<Writer> > output_vector;
-  output_vector.push_back(std::move(console_writer));
-  output_vector.push_back(std::move(file_writer));
-
-  std::unique_ptr<Writer> multiwriter(
-      new MultiWriter(std::move(output_vector)));
-
-  TextWriter::SetWriter(std::move(multiwriter));
+  SetVectorForOutput(std::move(console_writer), std::move(file_writer));
 }
 
 void OutputSetup::SetVectorForOutput(std::unique_ptr<Writer> console_output,
@@ -28,5 +24,13 @@ void OutputSetup::SetVectorForOutput(std::unique_ptr<Writer> console_output,
   output_vector.push_back(std::move(console_output));
   output_vector.push_back(std::move(file_output));
 
+  SetMultistream(std::move(output_vector));
+}
 
+void OutputSetup::SetMultistream(
+    std::vector<std::unique_ptr<Writer> > output_vector) {
+  std::unique_ptr<Writer> multiwriter(
+      new MultiWriter(std::move(output_vector)));
+
+  TextWriter::SetWriter(std::move(multiwriter));
 }
--- src/Console_output_processing/Output_configuration/output_setup.h
+++ src/Console_output_processing/Output_configuration/output_setup.h
@@ -1,9 +1,7 @@
 #ifndef OUTPUTSETUP_H
 #define OUTPUTSETUP_H
 
-#include "console_writer.h"
 #include "multiwriter.h"
-#include "text_writer.h"
 
 #include <string>
 
@@ -14,8 +12,11 @@ class OutputSetup {
   static void SetupTwoStreamsForOutput();
 
  private:
-  void SetVectorForOutput(std::unique_ptr<Writer> console_output,
-                          std::unique_ptr<Writer> file_output);
+  static void SetVectorForOutput(std::unique_ptr<Writer> console_output,
+                                 std::unique_ptr<Writer> file_output);
+
+  static void SetMultistream(
+      std::vector<std::unique_ptr<Writer> > output_vector);
 
   static std::string name_of_file_;
 };
