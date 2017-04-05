321e23796c533833b73fe9aa1e13edb4d2ff2526
OrdinaryMind <1.ordinarymind@gmail.com>
2017-04-05 22:53:45 +0300

    Add structure, that contain data about credit

---
+++ src/atm_operations/CreditOperations/credit_data.h
@@ -0,0 +1,10 @@
+﻿#ifndef CREDIT_DATA_H
+#define CREDIT_DATA_H
+
+struct CreditData {
+  int sum_of_credit_ = 0;
+  int amount_of_credit_month_ = 0;
+  double interest_rate_ = 0.0;
+};
+
+#endif  // CREDIT_DATA_H
