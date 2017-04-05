c09ab3b71f124a91ca9403a1fc58c20b20f54cfd
OrdinaryMind <1.ordinarymind@gmail.com>
2017-04-05 23:29:54 +0300

    Add class for credit worthiness check

---
+++ src/atm_operations/CreditOperations/creditworthiness.cpp
@@ -0,0 +1,3 @@
+﻿#include "creditworthiness.h"
+
+Creditworthiness::Creditworthiness() {}
---
+++ src/atm_operations/CreditOperations/creditworthiness.h
@@ -0,0 +1,9 @@
+﻿#ifndef CREDITWORTHINESS_H
+#define CREDITWORTHINESS_H
+
+class Creditworthiness {
+ public:
+  Creditworthiness();
+};
+
+#endif  // CREDITWORTHINESS_H
