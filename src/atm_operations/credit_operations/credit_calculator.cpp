4d2cc4b9255f4ca1169809f44f75541d8b3377c3
OrdinaryMind <1.ordinarymind@gmail.com>
2017-04-15 12:17:53 +0300

    Correct calculation credit monthly payment
    Multiplying special coefficient on sum of credit allows get
    correct annuity monthly credit payment.

--- src/atm_operations/credit_operations/credit_calculator.cpp
+++ src/atm_operations/credit_operations/credit_calculator.cpp
@@ -16,13 +16,18 @@ void CreditCalculator::CalculateCredit() { payments_.resize(credit_term_); }
 
 double CreditCalculator::AnnuityMonthlyPayment() {
   double persent_interest_rate_per_year = (interest_rate_ / 100) / 12;
+
   double annuity_coefficient_numerator =
       persent_interest_rate_per_year *
       std::pow((1 + persent_interest_rate_per_year), credit_term_);
+
   double annuity_coefficient_denumerator =
       std::pow(1 + persent_interest_rate_per_year, credit_term_) - 1;
+
   double annuity_coefficient =
       annuity_coefficient_numerator / annuity_coefficient_denumerator;
 
-  return annuity_coefficient;
+  double annuity_monthly_payment = annuity_coefficient * credit_sum_;
+
+  return annuity_monthly_payment;
 }
