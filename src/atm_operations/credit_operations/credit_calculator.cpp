a2637da215bce16e0b14ebed7c6a87821fa084ac
OrdinaryMind <1.ordinarymind@gmail.com>
2017-04-15 12:03:01 +0300

    Calculate monthly payment
    Method, that calculate annuity monthly payments,
    i.e the same credit payments all over the credit term.

--- src/atm_operations/credit_operations/credit_calculator.cpp
+++ src/atm_operations/credit_operations/credit_calculator.cpp
@@ -1,5 +1,7 @@
 ﻿#include "credit_calculator.h"
 
+#include <cmath>
+
 int CreditCalculator::CalculateMaxCreditSum(int cash) {
   return max_credit_sum_ = multiplier_ * cash;
 }
@@ -10,4 +12,17 @@ void CreditCalculator::SetCreditSum(int credit_sum) {
 
 void CreditCalculator::SetCreditTerm(int months) { credit_term_ = months; }
 
-void CreditCalculator::CalculateCredit() {}
+void CreditCalculator::CalculateCredit() { payments_.resize(credit_term_); }
+
+double CreditCalculator::AnnuityMonthlyPayment() {
+  double persent_interest_rate = (interest_rate_ / 100) / 12;
+  double annuity_coefficient_numerator =
+      persent_interest_rate *
+      std::pow((1 + persent_interest_rate), credit_term_);
+  double annuity_coefficient_denumerator =
+      std::pow(1 + persent_interest_rate, credit_term_) - 1;
+  double annuity_coefficient =
+      annuity_coefficient_numerator / annuity_coefficient_denumerator;
+
+  return annuity_coefficient;
+}
--- src/atm_operations/credit_operations/credit_calculator.h
+++ src/atm_operations/credit_operations/credit_calculator.h
@@ -1,6 +1,9 @@
 ﻿#ifndef CREDIT_CALCULATOR_H
 #define CREDIT_CALCULATOR_H
 
+#include <utility>
+#include <vector>
+
 class CreditCalculator {
  public:
   int CalculateMaxCreditSum(int cash);
@@ -11,11 +14,15 @@ class CreditCalculator {
   void CalculateCredit();
 
  private:
+  double AnnuityMonthlyPayment();
+
   int multiplier_ = 15;
   int max_credit_sum_ = 0;
   int credit_sum_ = 0;
   int credit_term_ = 0;
   double interest_rate_ = 14.0;
+
+  std::vector<std::pair<double, double>> payments_;
 };
 
 #endif  // CREDIT_CALCULATOR_H
