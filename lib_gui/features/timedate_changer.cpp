33ca0f29451f9af742e5b09159e90cdf95796806
OrdinaryMind <1.ordinarymind@gmail.com>
2017-04-26 20:22:25 +0300

    Change color of labels

--- lib_gui/features/timedate_changer.cpp
+++ lib_gui/features/timedate_changer.cpp
@@ -5,7 +5,7 @@
 #include <QTime>
 
 void TimeDateChanger::ChangeTimeData(QLabel* widget) {
-  QString current_data = QDate::currentDate().toString("dd.MM.yyyy") + "   " +
-                         QTime::currentTime().toString("hh:mm:ss");
-  widget->setText(current_data);
+  QString timedate = QDate::currentDate().toString("dd.MM.yyyy") + "   " +
+                     QTime::currentTime().toString("hh:mm:ss");
+  widget->setText(timedate);
 }
