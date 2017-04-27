54574f0dface9c420084de6bd32cd885930f8183
OrdinaryMind <1.ordinarymind@gmail.com>
2017-04-27 22:13:13 +0300

    Add interest rate generator class implementation

--- atm_core/credit_department/interest_rate_generator.cpp
+++ atm_core/credit_department/interest_rate_generator.cpp
@@ -1,3 +1,21 @@
 ﻿#include "interest_rate_generator.h"
 
-InterestRateGenerator::InterestRateGenerator() {}
+double InterestRateGenerator::kMaximalRate_ = 14.0;
+double InterestRateGenerator::kMinimalRate_ = 5.0;
+int InterestRateGenerator::kMinimalCash_ = 1000;
+int InterestRateGenerator::kMaximalCash_ = 20000;
+
+double InterestRateGenerator::GetInterestRate(double cash) {
+  if (cash == kMinimalCash_) {
+    return kMaximalRate_;
+  } else if (cash >= kMaximalCash_) {
+    return kMinimalRate_;
+  } else {
+    return GenerateInterestRate(cash);
+  }
+}
+
+double InterestRateGenerator::GenerateInterestRate(int cash) {
+  double interest_rate = (cash - kMinimalCash_) / kMinimalCash_;
+  return interest_rate;
+}
--- atm_core/credit_department/interest_rate_generator.h
+++ atm_core/credit_department/interest_rate_generator.h
@@ -3,7 +3,15 @@
 
 class InterestRateGenerator {
  public:
-  InterestRateGenerator();
+  static double GetInterestRate(double cash);
+  static double GenerateInterestRate(int cash);
+
+ private:
+  static int kMinimalCash_;
+  static int kMaximalCash_;
+
+  static double kMinimalRate_;
+  static double kMaximalRate_;
 };
 
 #endif  // INTEREST_RATE_GENERATOR_H
