fec4c7c8f54a75b3b0bcd0113c0256fa142bcd9e
OrdinaryMind <1.ordinarymind@gmail.com>
2017-04-27 23:10:16 +0300

    Save commit

--- lib_gui/initial_form/main_form.h
+++ lib_gui/initial_form/main_form.h
@@ -31,6 +31,7 @@ class MainForm : public QMainWindow {
   void RunTimers();
 
   Ui::MainForm* ui = nullptr;
+
   QTimer* color_timer_ = nullptr;
   QTimer* running_text_timer_ = nullptr;
   QTimer* time_date_timer_ = nullptr;
