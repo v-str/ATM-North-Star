e8cc9b73737959b758336ea8832b1ba6df175424
OrdinaryMind <1.ordinarymind@gmail.com>
2017-05-01 14:56:36 +0300

    Add identification data implementation

--- atm_core/user_data/identification_data.cpp
+++ atm_core/user_data/identification_data.cpp
@@ -1,3 +1,14 @@
 ﻿#include "identification_data.h"
 
-IdentificationData::IdentificationData() {}
+IdentificationData::IdentificationData()
+    : login_("user"), password_("password") {}
+
+IdentificationData::IdentificationData(const std::string& login,
+                                       const std::string& password)
+    : login_(login), password_(password) {}
+
+void IdentificationData::SetLogin(const std::string& login) { login_ = login; }
+
+void IdentificationData::SetPassword(const std::string& password) {
+  password_ = password;
+}
--- atm_core/user_data/identification_data.h
+++ atm_core/user_data/identification_data.h
@@ -1,9 +1,19 @@
 ﻿#ifndef IDENTIFICATION_DATA_H
 #define IDENTIFICATION_DATA_H
 
+#include <string>
+
 class IdentificationData {
  public:
   IdentificationData();
+  IdentificationData(const std::string& login, const std::string& password);
+
+  void SetLogin(const std::string& login);
+  void SetPassword(const std::string& password);
+
+ private:
+  std::string login_;
+  std::string password_;
 };
 
 #endif  // IDENTIFICATION_DATA_H
