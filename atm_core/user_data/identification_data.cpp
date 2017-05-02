4424536d1e1147c228775f024e9f532d2a486171
OrdinaryMind <1.ordinarymind@gmail.com>
2017-05-02 21:50:57 +0300

    Add missing part of code in operator=

--- atm_core/user_data/identification_data.cpp
+++ atm_core/user_data/identification_data.cpp
@@ -13,6 +13,8 @@ IdentificationData& IdentificationData::operator=(
     const IdentificationData& identification_data) {
   login_ = identification_data.Login();
   password_ = identification_data.Password();
+
+  return *this;
 }
 
 void IdentificationData::SetLogin(const std::string& login) { login_ = login; }
