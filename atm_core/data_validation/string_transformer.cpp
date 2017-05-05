e0f3787ea3df97a1bcb697acb55e39a4c27845f0
OrdinaryMind <1.ordinarymind@gmail.com>
2017-05-05 15:45:07 +0300

    Realize string data transformation to digits

---
+++ atm_core/data_validation/string_transformer.cpp
@@ -0,0 +1,33 @@
+﻿#include "string_transformer.h"
+
+#include <iostream>
+
+int StringTransformer::GetDigitalValueFromUser() {
+  return ConvertStringToDigit(GetStringFromUser());
+}
+
+std::string StringTransformer::GetStringFromUser() {
+  std::string user_string;
+  getline(std::cin, user_string);
+  return user_string;
+}
+
+int StringTransformer::ConvertStringToDigit(const std::string& string) {
+  if (StringNotEmpty(string)) {
+    try {
+      return std::stoi(string);
+    } catch (const std::invalid_argument&) {
+    } catch (const std::out_of_range&) {
+    }
+  }
+  return kInvalidValue;
+}
+
+bool StringTransformer::StringNotEmpty(const std::string& string) {
+  for (unsigned int i = 0; i < string.size(); ++i) {
+    if (!isdigit(string[i])) {
+      return false;
+    }
+  }
+  return !string.empty();
+}
---
+++ atm_core/data_validation/string_transformer.h
@@ -0,0 +1,18 @@
+﻿#ifndef STRING_TRANSFORMER_H
+#define STRING_TRANSFORMER_H
+
+#include <string>
+
+class StringTransformer {
+ public:
+  static int GetDigitalValueFromUser();
+  static std::string GetStringFromUser();
+
+ private:
+  static int ConvertStringToDigit(const std::string& string);
+  static bool StringNotEmpty(const std::string& string);
+
+  static const int kInvalidValue = -1;
+};
+
+#endif  // STRING_TRANSFORMER_H
