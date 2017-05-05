c4744ac6ef08e04a0bdc1622c7ca7cdbf0f69c88
OrdinaryMind <1.ordinarymind@gmail.com>
2017-05-05 11:47:26 +0300

    Add class for user data validation

---
+++ atm_core/atm_machine/data_validator.cpp
@@ -0,0 +1,10 @@
+﻿#include "data_validator.h"
+
+#include <cmath>
+
+bool DataValidator::IsIntegerSum(double sum) {
+  double integer_part = 0.0;
+  double fractional_part = std::modf(sum, &integer_part);
+
+  return fractional_part == 0.0;
+}
---
+++ atm_core/atm_machine/data_validator.h
@@ -0,0 +1,10 @@
+﻿#ifndef DATA_VALIDATOR_H
+#define DATA_VALIDATOR_H
+
+class DataValidator {
+ public:
+ private:
+  static bool IsIntegerSum(double sum);
+};
+
+#endif  // DATA_VALIDATOR_H
