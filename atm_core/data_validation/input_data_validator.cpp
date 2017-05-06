d420aa5f9ed429b00ec74ba404ba856825a0bcdd
OrdinaryMind <1.ordinarymind@gmail.com>
2017-05-06 19:51:21 +0300

    Add validation on correct refill sum

--- atm_core/data_validation/input_data_validator.cpp
+++ atm_core/data_validation/input_data_validator.cpp
@@ -11,6 +11,13 @@ bool InputDataValidator::IsPasswordLengthRight(const std::string& login) {
   return login.length() == k_password_length_;
 }
 
+bool InputDataValidator::IsRefillingCorrect(int refill_sum) {
+  if (IsIntegerValue(refill_sum)) {
+    return refill_sum >= k_min_refill_sum_ && refill_sum <= k_max_refill_sum_;
+  }
+  return false;
+}
+
 bool InputDataValidator::IsIntegerValue(double value) {
   double integer_part = k_null_;
   double fractional_part = std::modf(value, &integer_part);
--- atm_core/data_validation/input_data_validator.h
+++ atm_core/data_validation/input_data_validator.h
@@ -7,13 +7,21 @@ class InputDataValidator {
  public:
   static bool IsLoginLengthRight(const std::string& login);
   static bool IsPasswordLengthRight(const std::string& login);
-  static bool IsIntegerValue(double value);
+  static bool IsRefillingCorrect(int refill_sum);
 
  private:
+  static bool IsIntegerValue(double value);
+
   static constexpr int k_login_max_length_ = 35;
   static constexpr int k_login_min_length_ = 4;
   static constexpr int k_password_length_ = 4;
   static constexpr double k_null_ = 0.0;
+
+  static constexpr int k_min_refill_sum_ = 10;
+  static constexpr int k_max_refill_sum_ = 50000;
+
+  static constexpr int k_min_withdrawal_sum_ = 10;
+  static constexpr int k_max_withdrawal_sum_ = 10000;
 };
 
 #endif  // INPUT_DATA_VALIDATOR_H
