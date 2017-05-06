ae75521a406e631fb0cdca94286db50b26ad64d1
OrdinaryMind <1.ordinarymind@gmail.com>
2017-05-06 22:49:23 +0300

    Preliminary withdrawal class realization

--- atm_core/atm_operations/withdrawal.h
+++ atm_core/atm_operations/withdrawal.h
@@ -1,9 +1,11 @@
 ﻿#ifndef WITHDRAWAL_H
 #define WITHDRAWAL_H
 
+class AtmUser;
+
 class Withdrawal {
  public:
-  Withdrawal();
+  static void WithdrawCashFrom(AtmUser* user, int sum_of_withdrawal);
 };
 
 #endif  // WITHDRAWAL_H
