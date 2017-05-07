3c66e6d9ecfc0eecb95662e3df078220ac014c27
OrdinaryMind <1.ordinarymind@gmail.com>
2017-05-07 22:13:18 +0300

    Add preliminary version of exit widget

---
+++ lib_gui/initial_screen/exit_widget.cpp
@@ -0,0 +1,20 @@
+﻿#include <exit_widget.h>
+#include <ui_exit_widget.h>
+
+#include <QTimer>
+
+#include <initial_property_installer.h>
+
+ExitWidget::ExitWidget(QWidget* parent)
+    : QWidget(parent), ui(new Ui::ExitWidget) {
+  ui->setupUi(this);
+}
+
+ExitWidget::~ExitWidget() {
+  delete ui;
+  delete timer_;
+}
+
+void ExitWidget::SetInitialProperties() {
+  InitialPropertyInstaller::InstallInitialProperies(this, 300, 150);
+}
---
+++ lib_gui/initial_screen/exit_widget.h
@@ -0,0 +1,26 @@
+﻿#ifndef EXIT_WIDGET_H
+#define EXIT_WIDGET_H
+
+#include <QWidget>
+
+class QTimer;
+
+namespace Ui {
+class ExitWidget;
+}
+
+class ExitWidget : public QWidget {
+  Q_OBJECT
+
+ public:
+  explicit ExitWidget(QWidget* parent = nullptr);
+  ~ExitWidget();
+
+ private:
+  void SetInitialProperties();
+
+  Ui::ExitWidget* ui = nullptr;
+  QTimer* timer_ = nullptr;
+};
+
+#endif  // EXIT_WIDGET_H
