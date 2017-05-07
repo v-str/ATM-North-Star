d1f08ef5be24c0b9cf4a51513dbffcd0f4706b86
OrdinaryMind <1.ordinarymind@gmail.com>
2017-05-07 10:41:39 +0300

    Create duplicate of class
    Add duplicate of ConsoleEditor class for further transformation
    related with replacing of instances old ConsoleEditor to
    new static ConsoleEditor class.

---
+++ lib_console/output_configuration/console_editor_duplicate.cpp
@@ -0,0 +1,55 @@
+﻿#include "console_editor_duplicate.h"
+#include "console_editor.h"
+
+#include <chrono>
+#include <iostream>
+#include <thread>
+
+void ConsoleEditorDuplicate::Sleep(int latency_ms) {
+  std::this_thread::sleep_for(std::chrono::milliseconds(latency_ms));
+}
+
+void ConsoleEditorDuplicate::IgnoreCinLine() {
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+void ConsoleEditorDuplicate::ClearScreen() {
+  int system_status = system("clear");
+  if (system_status == -1) {
+    WriteSymbolsNTimes("\n", 50);
+  }
+}
+
+void ConsoleEditorDuplicate::WriteTextWithDelayPerSymbol(
+    const std::string& text, int delay) {
+  for (const auto symbol : text) {
+    std::string temp(1, symbol);
+
+    TextWriter::Write(temp);
+
+    Sleep(delay);
+  }
+}
+
+void ConsoleEditorDuplicate::WriteSymbolsNTimes(const std::string& symbol,
+                                                int n) {
+  for (int i = 0; i < n; ++i) {
+    TextWriter::Write(symbol);
+  }
+}
+
+void ConsoleEditorDuplicate::AddEmptyLineNTimes(int amount_of_empty_lines) {
+  for (int i = 0; i < amount_of_empty_lines; ++i) {
+    TextWriter::Write("\n");
+  }
+}
+
+void ConsoleEditorDuplicate::WriteText(const std::string& text) {
+  TextWriter::Write(text);
+}
+
+void ConsoleEditorDuplicate::WriteTextWithInterrupt(const std::string& text,
+                                                    int interrupt) {
+  WriteTextWithDelayPerSymbol(text);
+  Sleep(interrupt);
+}
---
+++ lib_console/output_configuration/console_editor_duplicate.h
@@ -0,0 +1,33 @@
+﻿#ifndef CONSOLE_EDITOR_DUPLICATE_H
+#define CONSOLE_EDITOR_DUPLICATE_H
+
+#include <sstream>
+#include <string>
+
+#include "text_writer.h"
+
+class ConsoleEditorDuplicate {
+ public:
+  static void ClearScreen();
+
+  static void Sleep(int latency_ms = 15);
+  static void IgnoreCinLine();
+  static void WriteTextWithDelayPerSymbol(const std::string& text,
+                                          int delay = 5);
+  static void WriteSymbolsNTimes(const std::string& symbol, int n);
+  static void AddEmptyLineNTimes(int amount_of_empty_lines);
+  static void WriteText(const std::string& text);
+  static void WriteTextWithInterrupt(const std::string& text, int interrupt);
+
+  template <typename T>
+  static std::string ConvertValueToString(const T& value);
+};
+
+template <typename T>
+std::string ConsoleEditorDuplicate::ConvertValueToString(const T& value) {
+  std::stringstream stream;
+  stream << value;
+  return stream.str();
+}
+
+#endif  // CONSOLE_EDITOR_DUPLICATE_H
