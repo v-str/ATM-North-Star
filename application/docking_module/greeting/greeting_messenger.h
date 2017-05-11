634056ebc886789552ce8fa80c28f872b230ff79
OrdinaryMind <1.ordinarymind@gmail.com>
2017-05-11 15:48:51 +0300

    Add initial welcome message

--- application/docking_module/greeting/greeting_messenger.h
+++ application/docking_module/greeting/greeting_messenger.h
@@ -1,12 +1,15 @@
 ﻿#ifndef GREETING_MESSENGER_H
 #define GREETING_MESSENGER_H
 
+#include <string>
+
 class GreetingMessenger {
  public:
   static void ShowGreetingMessage();
 
  private:
   static void DrawStrip(const char strip_symbol, int strip_length);
+  static void AddString(const char strip_symbol, const std::string& string);
 
   static constexpr int strip_length_ = 50;
 };
