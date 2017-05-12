85fb584b6af44f4341144c100e65bdafe80a8404
OrdinaryMind <1.ordinarymind@gmail.com>
2017-05-12 23:46:09 +0300

    Refactoring user initial input process

--- application/docking_module/greeting/initial_input.cpp
+++ application/docking_module/greeting/initial_input.cpp
@@ -1,25 +1,19 @@
 ﻿#include "initial_input.h"
 
 #include <iostream>
-#include <string>
 
 int InitialInput::GetValueFromUser() {
-  std::string user_enter_string = "";
-  int final_user_value = 0;
+  std::string user_enter_string = GetString();
 
-  std::getline(std::cin, user_enter_string);
-  if (user_enter_string == "Q" || user_enter_string == "q") return kExit;
-
-  for (unsigned int i = 0; i < user_enter_string.size(); ++i) {
-    if (!isdigit(user_enter_string[i])) {
-      return kInvalidChoice;
-    }
+  if (IsContainExitCharacter(user_enter_string)) {
+    return kExit;
   }
 
-  try {
-    return std::stoi(user_enter_string);
-  } catch (const std::invalid_argument&) {
-  } catch (const std::out_of_range&) {
+  int final_user_value = 0;
+  if (IsDigit(user_enter_string)) {
+    final_user_value = ConvertToDigit(user_enter_string);
+  } else {
+    return kInvalidChoice;
   }
 
   if (final_user_value == kConsoleMode || final_user_value == kGuiMode) {
@@ -27,4 +21,33 @@ int InitialInput::GetValueFromUser() {
   }
 
   return kInvalidChoice;
+}
+
+std::string InitialInput::GetString() {
+  std::string temp_string;
+  std::getline(std::cin, temp_string);
+  return temp_string;
+}
+
+bool InitialInput::IsContainExitCharacter(const std::string& user_string) {
+  return user_string == "Q" || user_string == "q";
+}
+
+bool InitialInput::IsDigit(const std::string& user_string) {
+  for (unsigned int i = 0; i < user_string.size(); ++i) {
+    if (!isdigit(user_string[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+int InitialInput::ConvertToDigit(const std::string& user_string) {
+  try {
+    return std::stoi(user_string);
+  } catch (const std::invalid_argument&) {
+    return kInvalidChoice;
+  } catch (const std::out_of_range&) {
+    return kInvalidChoice;
+  }
 }
--- application/docking_module/greeting/initial_input.h
+++ application/docking_module/greeting/initial_input.h
@@ -1,11 +1,18 @@
 ﻿#ifndef INITIAL_INPUT_H
 #define INITIAL_INPUT_H
 
+#include <string>
+
 class InitialInput {
  public:
   static int GetValueFromUser();
 
  private:
+  static std::string GetString();
+  static bool IsContainExitCharacter(const std::string& user_string);
+  static bool IsDigit(const std::string& user_string);
+  static int ConvertToDigit(const std::string& user_string);
+
   static constexpr int kConsoleMode = 1;
   static constexpr int kGuiMode = 2;
   static constexpr int kExit = 0;
