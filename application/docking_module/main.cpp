8f60ec899800ac458fcefbe3fd17a4f2eabf9706
OrdinaryMind <1.ordinarymind@gmail.com>
2017-05-15 10:52:27 +0300

    Launch program modes in specialized class

--- application/docking_module/main.cpp
+++ application/docking_module/main.cpp
@@ -4,26 +4,23 @@
 #include <gui_mode.h>
 #include <mode_selector.h>
 
+#include <mode_launcher.h>
+
 int main(int argc, char* argv[]) {
   enum ProgramMode { kConsoleMode = 1, kGuiMode };
 
-  ModeSelector mode_selector;
-  mode_selector.SuggestMode();
+  ModeSelector selector;
+  ModeLauncher launcher;
 
-  switch (mode_selector.GetMode()) {
-    case kConsoleMode: {
-      ConsoleMode console_mode;
-      console_mode.RunInitialScreen();
+  selector.SuggestMode();
+
+  switch (selector.GetMode()) {
+    case kConsoleMode:
+      launcher.LaunchConsoleMode();
       break;
-    }
-    case kGuiMode: {
-      QApplication app(argc, argv);
-      Q_INIT_RESOURCE(atm_resources);
-      GuiMode gui_mode;
-      gui_mode.RunInitialScreen();
-      app.exec();
+    case kGuiMode:
+      launcher.LaunchGuiMode(argc, argv);
       break;
-    }
     default:
       break;
   }
