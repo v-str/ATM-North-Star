4ccdc87a547cbf3cc16f31f6aa056d8609a8450b
OrdinaryMind <1.ordinarymind@gmail.com>
2017-05-21 13:35:29 +0300

    Add method for calculation of alphabet symbols

--- atm/incoming_data_processing/string_analyzer.cpp
+++ atm/incoming_data_processing/string_analyzer.cpp
@@ -1,6 +1,7 @@
 ﻿#include <string_analyzer.h>
 
 int StringAnalyzer::digit_amount_ = 0;
+int StringAnalyzer::alphabet_symbol_amount_ = 0;
 
 int StringAnalyzer::AmountOfDigits(const std::string& string) {
   for (auto i : string) {
@@ -9,4 +10,13 @@ int StringAnalyzer::AmountOfDigits(const std::string& string) {
     }
   }
   return digit_amount_;
+}
+
+int StringAnalyzer::AmountOfAlphabeticSymbol(const std::string& string) {
+  for (auto i : string) {
+    if (isalpha(string[i])) {
+      alphabet_symbol_amount_++;
+    }
+  }
+  return alphabet_symbol_amount_;
 }
--- atm/incoming_data_processing/string_analyzer.h
+++ atm/incoming_data_processing/string_analyzer.h
@@ -6,9 +6,11 @@
 class StringAnalyzer {
  public:
   static int AmountOfDigits(const std::string& string);
+  static int AmountOfAlphabeticSymbol(const std::string& string);
 
  private:
   static int digit_amount_;
+  static int alphabet_symbol_amount_;
 };
 
 #endif  // STRING_ANALYZER_H
