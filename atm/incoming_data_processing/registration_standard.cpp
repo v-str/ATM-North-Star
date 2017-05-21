28fe3dce92d2f2df93eb03de853c9c4ac5bafb8f
OrdinaryMind <1.ordinarymind@gmail.com>
2017-05-21 19:43:13 +0300

    Add standard of password length

--- atm/incoming_data_processing/registration_standard.cpp
+++ atm/incoming_data_processing/registration_standard.cpp
@@ -3,3 +3,5 @@
 int RegistrationStandard::MinLoginLength() { return k_min_login_length_; }
 
 int RegistrationStandard::MaxLoginLength() { return k_max_login_length_; }
+
+int RegistrationStandard::PasswordLength() { return k_password_length_; }
--- atm/incoming_data_processing/registration_standard.h
+++ atm/incoming_data_processing/registration_standard.h
@@ -5,10 +5,12 @@ class RegistrationStandard {
  public:
   static int MinLoginLength();
   static int MaxLoginLength();
+  static int PasswordLength();
 
  private:
   static constexpr int k_min_login_length_ = 4;
   static constexpr int k_max_login_length_ = 35;
+  static constexpr int k_password_length_ = 6;
 };
 
 #endif  // REGISTRATION_STANDARD_H
