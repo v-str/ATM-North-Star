8682a5d32eea9545328f9626376a9b65b5a21e1b
OrdinaryMind <1.ordinarymind@gmail.com>
2017-05-30 14:32:04 +0300

    Add initial menu class realization

---
+++ application/console_version/initial_menu.h
@@ -0,0 +1,20 @@
+﻿#ifndef INITIAL_MENU_H
+#define INITIAL_MENU_H
+
+#include <quit_menu_handler.h>
+
+class InitialMenu {
+ public:
+  void RunInitialMenu();
+
+  int UserChoice() const;
+
+ private:
+  void ShowInitialScreen();
+
+  QuitMenuHandler quit_handler_;
+
+  int user_choice_ = -1;
+};
+
+#endif  // INITIAL_MENU_H
