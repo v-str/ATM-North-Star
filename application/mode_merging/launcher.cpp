5c1d2a4d1d77df31f8821753a6fd825b790cd21f
OrdinaryMind <1.ordinarymind@gmail.com>
2017-06-03 15:14:01 +0300

    Initialize object through smart pointer

--- application/mode_merging/launcher.cpp
+++ application/mode_merging/launcher.cpp
@@ -3,11 +3,15 @@
 #include <QApplication>
 
 #include <gui_mode.h>
-#include <initial_menu.h>
+
+Launcher::Launcher() : initial_menu_(new InitialMenu) {}
 
 void Launcher::LaunchConsoleMode() {
-  InitialMenu console_initial_menu;
-  console_initial_menu.RunInitialMenu();
+  // InitialMenu console_initial_menu;
+
+  initial_menu_->RunInitialMenu();
+
+  // console_initial_menu.RunInitialMenu();
 }
 
 void Launcher::LaunchGuiMode(int argc, char* argv[]) {
