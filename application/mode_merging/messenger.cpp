5cb10e3bd3e577f385a7e9e06a74c3f2fd574065
OrdinaryMind <1.ordinarymind@gmail.com>
2017-06-03 15:01:59 +0300

    Change initial screen appearance

--- application/mode_merging/messenger.cpp
+++ application/mode_merging/messenger.cpp
@@ -4,27 +4,52 @@
 
 void Messenger::DisplayLauncherMessage() {
   DisplayVersion();
-
-  std::cout << "\n\nATM system \"North Star\"\n\n\n"
-               "Choose the interface:\n\n"
-               "1.Console \n"
-               "2.Graphical \n\n"
-               "(\'q\' for quit)\n\n"
-               "Enter: ";
+  DisplayATMName();
+  ModeSuggestion();
 }
 
 void Messenger::IncorrectInput() {
-  std::cout << "Incorrect input, please re-enter: ";
+  std::cout << "# Incorrect input, please re-enter: ";
+}
+
+void Messenger::DisplayATMName() {
+  AddNewLine();
+  AddNewLine();
+  DrawStrip(greeting_strip_length_);
+  AddNewLine();
+  std::cout << "#           ATM \"North star\"           #\n";
+  DrawStrip(greeting_strip_length_);
+  AddNewLine();
 }
 
 void Messenger::DisplayVersion() {
   DrawStrip(version_strip_length_);
-  std::cout << "\n-     v.1.0.1      -\n";
+  AddNewLine();
+  std::cout << "#     v.1.0.1      #";
+  AddNewLine();
   DrawStrip(version_strip_length_);
+}
+
+void Messenger::ModeSuggestion() {
+  AddNewLine();
+  DrawStrip(greeting_strip_length_);
+  AddNewLine();
+  std::cout << "#                                      #\n"
+               "# Choose the interface:                #\n"
+               "# 1.Console                            #\n"
+               "# 2.Graphical                          #\n"
+               "# (\'q\' for quit)                       #\n"
+               "#                                      #\n";
+  DrawStrip(greeting_strip_length_);
+  AddNewLine();
+  AddNewLine();
+  std::cout << "# Enter: ";
 }
 
 void Messenger::DrawStrip(int length_of_strip) {
   for (int i = 0; i < length_of_strip; ++i) {
-    std::cout << "-";
+    std::cout << "#";
   }
 }
+
+void Messenger::AddNewLine() { std::cout << "\n"; }
--- application/mode_merging/messenger.h
+++ application/mode_merging/messenger.h
@@ -9,10 +9,14 @@ class Messenger {
   static void IncorrectInput();
 
  private:
+  static void DisplayATMName();
   static void DisplayVersion();
+  static void ModeSuggestion();
 
   static void DrawStrip(int length_of_strip);
+  static void AddNewLine();
 
+  static constexpr int greeting_strip_length_ = 40;
   static constexpr int version_strip_length_ = 20;
 };
 
