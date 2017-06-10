8bb4c83dded2190763a60125982076bdbf6a9fe3
OrdinaryMind <1.ordinarymind@gmail.com>
2017-06-10 21:02:43 +0300

    Add labels set painting

--- lib_gui/widgets_appearance/widget_color_designer.cpp
+++ lib_gui/widgets_appearance/widget_color_designer.cpp
@@ -1,7 +1,17 @@
-﻿#include "widget_color_designer.h"
+﻿#include <widget_color_designer.h>
+
+#include <QLabel>
+
+#include <painter.h>
 
 WidgetColorDesigner::WidgetColorDesigner() {}
 
 void WidgetColorDesigner::CustomizeColorSet(const WidgetColor& widget_color) {
   widget_color_ = widget_color;
+}
+
+void WidgetColorDesigner::PaintWidgetSet(QList<QLabel*> label_list) const {
+  for (auto label = label_list.begin(); label != label_list.end(); ++label) {
+    Painter::ChangeLabelColor(*label, widget_color_.MainColor());
+  }
 }
--- lib_gui/widgets_appearance/widget_color_designer.h
+++ lib_gui/widgets_appearance/widget_color_designer.h
@@ -1,13 +1,19 @@
 ﻿#ifndef WIDGET_COLOR_DESIGNER_H
 #define WIDGET_COLOR_DESIGNER_H
 
+#include <QList>
+
 #include <widget_color.h>
+
+class QLabel;
 
 class WidgetColorDesigner {
  public:
   WidgetColorDesigner();
 
   void CustomizeColorSet(const WidgetColor& widget_color);
+
+  void PaintWidgetSet(QList<QLabel*> label_list) const;
 
  private:
   WidgetColor widget_color_;
