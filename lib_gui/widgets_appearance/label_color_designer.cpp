d7b5738a8b1990edff4fdec1f76af1e50e2ce5a0
OrdinaryMind <1.ordinarymind@gmail.com>
2017-06-10 12:46:23 +0300

    Add class for label colorizing

---
+++ lib_gui/widgets_appearance/label_color_designer.cpp
@@ -0,0 +1,6 @@
+﻿#include "label_color_designer.h"
+
+LabelColorDesigner::LabelColorDesigner(QList<QWidget*> label_list)
+    : ColorDesigner(label_list) {}
+
+void LabelColorDesigner::PaintWidgets() {}
---
+++ lib_gui/widgets_appearance/label_color_designer.h
@@ -0,0 +1,14 @@
+﻿#ifndef LABEL_COLOR_DESIGNER_H
+#define LABEL_COLOR_DESIGNER_H
+
+#include <color_designer.h>
+
+class LabelColorDesigner : public ColorDesigner {
+ public:
+  LabelColorDesigner(QList<QWidget*> label_list);
+  ~LabelColorDesigner();
+
+  virtual void PaintWidgets();
+};
+
+#endif  // LABEL_COLOR_DESIGNER_H
