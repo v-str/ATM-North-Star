d449f00478712cf362a48c59c11a8dc6967d4eb3
OrdinaryMind <1.ordinarymind@gmail.com>
2017-06-12 20:36:22 +0300

    Add constructor for FontSizeSetter class

--- lib_gui/widgets_arrangement/font_settings/size_font_setter.cpp
+++ lib_gui/widgets_arrangement/font_settings/size_font_setter.cpp
@@ -1,5 +1,8 @@
 ﻿#include <size_font_setter.h>
 
+SizeFontSetter::SizeFontSetter(int scale_coefficient, int font_size_gain)
+    : scale_coefficient_(scale_coefficient), font_size_gain_(font_size_gain) {}
+
 void SizeFontSetter::SetScaleCoefficient(int scale_coefficient) {
   scale_coefficient_ = scale_coefficient;
 }
--- lib_gui/widgets_arrangement/font_settings/size_font_setter.h
+++ lib_gui/widgets_arrangement/font_settings/size_font_setter.h
@@ -1,12 +1,18 @@
 ﻿#ifndef SIZE_FONT_SETTER_H
 #define SIZE_FONT_SETTER_H
 
+#include <QFont>
+
 class SizeFontSetter {
  public:
+  SizeFontSetter(int scale_coefficient, int font_size_gain);
+
   void SetScaleCoefficient(int scale_coefficient);
   void SetFontSizeGain(int font_size_gain);
 
  private:
+  QFont widget_font_;
+
   int scale_coefficient_ = 0;
   int font_size_gain_ = 1;
 };
