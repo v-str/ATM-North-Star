11dd34645d2321621d03228708348e8cbdbdd405
OrdinaryMind <1.ordinarymind@gmail.com>
2017-06-12 19:12:38 +0300

    Add variables for font size changing

--- lib_gui/widgets_arrangement/font_settings/size_font_setter.cpp
+++ lib_gui/widgets_arrangement/font_settings/size_font_setter.cpp
@@ -1,3 +1,9 @@
 ﻿#include <size_font_setter.h>
 
-SizeFontSetter::SizeFontSetter() {}
+void SizeFontSetter::SetScaleCoefficient(int scale_coefficient) {
+  scale_coefficient_ = scale_coefficient;
+}
+
+void SizeFontSetter::SetFontSizeGain(int font_size_gain) {
+  font_size_gain_ = font_size_gain;
+}
--- lib_gui/widgets_arrangement/font_settings/size_font_setter.h
+++ lib_gui/widgets_arrangement/font_settings/size_font_setter.h
@@ -3,7 +3,12 @@
 
 class SizeFontSetter {
  public:
-  SizeFontSetter();
+  void SetScaleCoefficient(int scale_coefficient);
+  void SetFontSizeGain(int font_size_gain);
+
+ private:
+  int scale_coefficient_ = 0;
+  int font_size_gain_ = 1;
 };
 
 #endif  // SIZE_FONT_SETTER_H
