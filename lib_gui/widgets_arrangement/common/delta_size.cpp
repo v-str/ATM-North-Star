97822d81cbe27a66245654aa13e5e9f32e7671fd
OrdinaryMind <1.ordinarymind@gmail.com>
2017-06-28 21:30:37 +0300

    Add setters and getters for DeltaSize class

--- lib_gui/widgets_arrangement/common/delta_size.cpp
+++ lib_gui/widgets_arrangement/common/delta_size.cpp
@@ -2,3 +2,13 @@
 
 DeltaSize::DeltaSize(int delta_width, int delta_height)
     : delta_width_(delta_width), delta_height_(delta_height) {}
+
+void DeltaSize::SetDeltaWidth(int delta_width) { delta_width_ = delta_width; }
+
+void DeltaSize::SetDeltaHeight(int delta_height) {
+  delta_height_ = delta_height;
+}
+
+int DeltaSize::DeltaWidth() const { return delta_width_; }
+
+int DeltaSize::DeltaHeight() const { return delta_height_; }
--- lib_gui/widgets_arrangement/common/delta_size.h
+++ lib_gui/widgets_arrangement/common/delta_size.h
@@ -7,6 +7,12 @@ class DeltaSize {
  public:
   DeltaSize(int delta_width, int delta_height);
 
+  void SetDeltaWidth(int delta_width);
+  void SetDeltaHeight(int delta_height);
+
+  int DeltaWidth() const;
+  int DeltaHeight() const;
+
  private:
   int delta_width_ = 0;
   int delta_height_ = 0;
