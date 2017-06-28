96a1be36e52f64c182426e9732e21c84126b702a
OrdinaryMind <1.ordinarymind@gmail.com>
2017-06-28 21:20:11 +0300

    Add class for delta size storing

---
+++ lib_gui/widgets_arrangement/common/delta_size.cpp
@@ -0,0 +1,4 @@
+﻿#include <delta_size.h>
+
+DeltaSize::DeltaSize(int delta_width, int delta_height)
+    : delta_width_(delta_width), delta_height_(delta_height) {}
---
+++ lib_gui/widgets_arrangement/common/delta_size.h
@@ -0,0 +1,15 @@
+﻿#ifndef DELTA_SIZE_H
+#define DELTA_SIZE_H
+
+#include <QSize>
+
+class DeltaSize {
+ public:
+  DeltaSize(int delta_width, int delta_height);
+
+ private:
+  int delta_width_ = 0;
+  int delta_height_ = 0;
+};
+
+#endif  // DELTA_SIZE_H
