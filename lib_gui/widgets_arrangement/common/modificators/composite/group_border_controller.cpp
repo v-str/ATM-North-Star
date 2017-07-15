682d809922a02cb48907f21af510a0386be35c63
OrdinaryMind <1.ordinarymind@gmail.com>
2017-07-15 18:57:38 +0300

    Add setter for widgets interval setting
    Write pseudo code for task simplifying

--- lib_gui/widgets_arrangement/common/modificators/composite/group_border_controller.cpp
+++ lib_gui/widgets_arrangement/common/modificators/composite/group_border_controller.cpp
@@ -1,6 +1,23 @@
 ﻿#include <group_border_controller.h>
 
+#include <QWidget>
+
 void GroupBorderController::SetGroupMode(
     GroupBorderController::GroupMode group_mode) {
   group_mode_ = group_mode;
+}
+
+void GroupBorderController::ControlGroup(QVector<QWidget*>& widget_vector) {
+  // Control head of group
+  // if (IsBorderOverstepped()) {
+  // control body of the group
+  // switch (group_mode) {
+  // case kVertical: control vertical group
+  // break;
+  // case kGorizontal: control gorizontal group
+  // break;
+}
+
+void GroupBorderController::SetWidgetInterval(int widget_interval) {
+  widget_interval_ = widget_interval;
 }
--- lib_gui/widgets_arrangement/common/modificators/composite/group_border_controller.h
+++ lib_gui/widgets_arrangement/common/modificators/composite/group_border_controller.h
@@ -1,7 +1,11 @@
 ﻿#ifndef GROUP_BORDER_CONTROLLER_H
 #define GROUP_BORDER_CONTROLLER_H
 
+#include <QVector>
+
 #include <border_controller.h>
+
+class QWidget;
 
 class GroupBorderController : public BorderController {
  public:
@@ -9,8 +13,14 @@ class GroupBorderController : public BorderController {
 
   void SetGroupMode(GroupMode group_mode);
 
+  void ControlGroup(QVector<QWidget*>& widget_vector);
+
+  void SetWidgetInterval(int widget_interval);
+
  private:
   GroupMode group_mode_;
+
+  int widget_interval_ = 10;
 };
 
 #endif  // GROUP_BORDER_CONTROLLER_H
