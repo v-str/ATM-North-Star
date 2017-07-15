29bf332e2de9dbc8255997dd6bfc64612cda9890
OrdinaryMind <1.ordinarymind@gmail.com>
2017-07-15 19:04:24 +0300

    Add carcass for group widgets border control

--- lib_gui/widgets_arrangement/common/modificators/composite/group_border_controller.cpp
+++ lib_gui/widgets_arrangement/common/modificators/composite/group_border_controller.cpp
@@ -9,13 +9,19 @@ void GroupBorderController::SetGroupMode(
 
 void GroupBorderController::ControlGroup(QVector<QWidget*>& widget_vector) {
   ControlGroupHead(widget_vector[0]);
-  // if (IsBorderOverstepped()) {
-  // control body of the group
-  // switch (group_mode) {
-  // case kVertical: control vertical group
-  // break;
-  // case kGorizontal: control gorizontal group
-  // break;
+
+  if (IsBorderOverstepped()) {
+    switch (group_mode_) {
+      case kVertical:
+        for (int i = 1; i < widget_vector.size(); ++i) {
+        }
+        break;
+      case kHorizontal:
+        for (int i = 1; i < widget_vector.size(); ++i) {
+        }
+        break;
+    }
+  }
 }
 
 void GroupBorderController::SetWidgetInterval(int widget_interval) {
