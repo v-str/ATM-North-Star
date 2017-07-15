1848da4cde806c7abcad3048f32ac2bda6114fb2
OrdinaryMind <1.ordinarymind@gmail.com>
2017-07-15 22:36:06 +0300

    Add class for storing limited widget geometry

---
+++ lib_gui/widgets_arrangement/common/widgets_geometries/widget_geometry_limiter.cpp
@@ -0,0 +1,1 @@
+﻿#include <widget_geometry_limiter.h>
---
+++ lib_gui/widgets_arrangement/common/widgets_geometries/widget_geometry_limiter.h
@@ -0,0 +1,8 @@
+﻿#ifndef WIDGET_GEOMETRY_LIMITER_H
+#define WIDGET_GEOMETRY_LIMITER_H
+
+class WidgetGeometryLimiter {
+ public:
+};
+
+#endif  // WIDGET_GEOMETRY_LIMITER_H
