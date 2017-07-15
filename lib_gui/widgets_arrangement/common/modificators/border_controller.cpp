81f61bb9d6a12c1605868c49832c9d6ca0ac41ac
OrdinaryMind <1.ordinarymind@gmail.com>
2017-07-15 14:55:23 +0300

    Add bodies of control border sides methods

--- lib_gui/widgets_arrangement/common/modificators/border_controller.cpp
+++ lib_gui/widgets_arrangement/common/modificators/border_controller.cpp
@@ -18,10 +18,38 @@ void BorderController::ControlModifiableWidget(QWidget* widget) {
   widget->setGeometry(modifiable_widget_position_);
 }
 
-void BorderController::ControlUp() {}
+void BorderController::ControlUp() {
+  int up_limit =
+      parent_size_.y() + modifiable_widget_position_.height() + kBorderSpacer;
 
-void BorderController::ControlDown() {}
+  if (modifiable_widget_position_.y() < up_limit) {
+    modifiable_widget_position_.setY(up_limit);
+  }
+}
 
-void BorderController::ControlLeft() {}
+void BorderController::ControlDown() {
+  int down_limit = parent_size_.height() -
+                   modifiable_widget_position_.height() - kBorderSpacer;
 
-void BorderController::ControlRight() {}
+  if (modifiable_widget_position_.y() > down_limit) {
+    modifiable_widget_position_.setY(down_limit);
+  }
+}
+
+void BorderController::ControlLeft() {
+  int left_limit =
+      parent_size_.x() + modifiable_widget_position_.width() + kBorderSpacer;
+
+  if (modifiable_widget_position_.x() < left_limit) {
+    modifiable_widget_position_.setX(left_limit);
+  }
+}
+
+void BorderController::ControlRight() {
+  int right_limit = parent_size_.width() - modifiable_widget_position_.width() -
+                    kBorderSpacer;
+
+  if (modifiable_widget_position_.x() > right_limit) {
+    modifiable_widget_position_.setX(right_limit);
+  }
+}
