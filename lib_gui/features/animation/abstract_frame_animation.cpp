35796ab295f5924b367e571eb5e5cffb9c3164cb
OrdinaryMind <1.ordinarymind@gmail.com>
2017-07-26 18:34:29 +0300

    Add animation of property class to the base class

--- lib_gui/features/animation/abstract_frame_animation.cpp
+++ lib_gui/features/animation/abstract_frame_animation.cpp
@@ -1,6 +1,11 @@
 ﻿#include <abstract_frame_animation.h>
 
+#include <QPropertyAnimation>
 #include <QWidget>
 
 AbstractFrameAnimation::AbstractFrameAnimation(QWidget* parent)
-    : QObject(parent) {}
+    : QObject(parent), property_animation_(new QPropertyAnimation) {}
+
+AbstractFrameAnimation::~AbstractFrameAnimation() {
+  delete property_animation_;
+}
--- lib_gui/features/animation/abstract_frame_animation.h
+++ lib_gui/features/animation/abstract_frame_animation.h
@@ -3,16 +3,21 @@
 
 #include <QObject>
 
+class QPropertyAnimation;
 class QWidget;
 
 class AbstractFrameAnimation : public QObject {
   Q_OBJECT
  public:
   explicit AbstractFrameAnimation(QWidget* parent = nullptr);
+  ~AbstractFrameAnimation();
 
  signals:
 
  public slots:
+
+ private:
+  QPropertyAnimation* property_animation_ = nullptr;
 };
 
 #endif  // ABSTRACT_FRAME_ANIMATION_H
