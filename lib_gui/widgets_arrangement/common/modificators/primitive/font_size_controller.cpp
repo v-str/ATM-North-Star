6ed8a29d3a3d53b0d6a3ce04787a096bb97da93f
OrdinaryMind <1.ordinarymind@gmail.com>
2017-08-05 19:15:30 +0300

    Add realization of font size controller class

--- lib_gui/widgets_arrangement/common/modificators/primitive/font_size_controller.cpp
+++ lib_gui/widgets_arrangement/common/modificators/primitive/font_size_controller.cpp
@@ -1,3 +1,20 @@
 ﻿#include <font_size_controller.h>
 
-FontSizeController::FontSizeController() {}
+#include <QFont>
+#include <QWidget>
+
+FontSizeController::FontSizeController(int initial_font_size,
+                                       int divide_coefficient_per_scale)
+    : initial_font_size_(initial_font_size),
+      divide_coefficient_per_scale_(divide_coefficient_per_scale) {}
+
+void FontSizeController::ControlFontSize(int current_widget_width,
+                                         QWidget* widget) {
+  int grows_coefficient = current_widget_width / divide_coefficient_per_scale_;
+  int font_size = initial_font_size_ + grows_coefficient;
+
+  QFont new_font = widget->font();
+  new_font.setPointSize(font_size);
+
+  widget->setFont(new_font);
+}
--- lib_gui/widgets_arrangement/common/modificators/primitive/font_size_controller.h
+++ lib_gui/widgets_arrangement/common/modificators/primitive/font_size_controller.h
@@ -1,9 +1,18 @@
 ﻿#ifndef FONT_SIZE_CONTROLLER_H
 #define FONT_SIZE_CONTROLLER_H
 
+class QWidget;
+
 class FontSizeController {
  public:
-  FontSizeController();
+  FontSizeController(int initial_font_size = 10,
+                     int divide_coefficient_per_scale = 50);
+
+  void ControlFontSize(int current_widget_width, QWidget* widget);
+
+ private:
+  int initial_font_size_ = 0;
+  int divide_coefficient_per_scale_ = 0;
 };
 
 #endif  // FONT_SIZE_CONTROLLER_H
