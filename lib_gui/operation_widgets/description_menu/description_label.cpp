789592c50b7e858714bfd2ad85822c559b74ea91
OrdinaryMind <1.ordinarymind@gmail.com>
2017-08-05 19:31:25 +0300

    Perform size control for description label
    Carry label content by words

--- lib_gui/operation_widgets/description_menu/description_label.cpp
+++ lib_gui/operation_widgets/description_menu/description_label.cpp
@@ -1,14 +1,22 @@
 ﻿#include <description_label.h>
 
+#include <QResizeEvent>
+
 #include <application_color.h>
 #include <atm_color_designer.h>
 #include <description_menu_geometry.h>
 #include <description_text.h>
+#include <font_size_controller.h>
 
 DescriptionLabel::DescriptionLabel(QWidget* parent)
     : QLabel(parent), color_designer_(new AtmColorDesigner) {
   setAlignment(Qt::AlignLeft | Qt::AlignTop);
   setGeometry(DescriptionMenuGeometry::DescriprionLabel());
+
+  setWordWrap(true);
+  font_size_controller_ =
+      new FontSizeController(kInitialFontSize, kDivideFontSizeCoefficient);
+
   ColorizeWidget();
 }
 
@@ -32,6 +40,10 @@ void DescriptionLabel::ShowWithdrawalInfo() {
 
 void DescriptionLabel::ShowStatementInfo() {
   setText(DescriptionText::StatementIndoText());
+}
+
+void DescriptionLabel::resizeEvent(QResizeEvent* event) {
+  font_size_controller_->ControlFontSize(event->size().width(), this);
 }
 
 void DescriptionLabel::ColorizeWidget() {
--- lib_gui/operation_widgets/description_menu/description_label.h
+++ lib_gui/operation_widgets/description_menu/description_label.h
@@ -5,6 +5,7 @@
 
 class QWidget;
 class AtmColorDesigner;
+class FontSizeController;
 
 class DescriptionLabel : public QLabel {
   Q_OBJECT
@@ -19,10 +20,17 @@ class DescriptionLabel : public QLabel {
   void ShowWithdrawalInfo();
   void ShowStatementInfo();
 
+ protected:
+  void resizeEvent(QResizeEvent* event);
+
  private:
   void ColorizeWidget();
 
   AtmColorDesigner* color_designer_ = nullptr;
+  FontSizeController* font_size_controller_ = nullptr;
+
+  static const int kInitialFontSize = 11;
+  static const int kDivideFontSizeCoefficient = 50;
 };
 
 #endif  // DESCRIPTION_LABEL_H
