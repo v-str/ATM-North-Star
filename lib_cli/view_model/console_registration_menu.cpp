282c1a1843d0676bf6a0eb8369e20340aba95a36
OrdinaryMind <1.ordinarymind@gmail.com>
2017-08-16 21:36:16 +0300

    Add input handlers for registration menu

--- lib_cli/view_model/console_registration_menu.cpp
+++ lib_cli/view_model/console_registration_menu.cpp
@@ -1,6 +1,13 @@
 ﻿#include <console_registration_menu.h>
 
+#include <menu_input_handler.h>
 #include <registration_messenger.h>
+#include <user_input_handler.h>
+
+ConsoleRegistrationMenu::ConsoleRegistrationMenu()
+    : user_input_(new UserInputHandler), menu_input_(new MenuInputHandler) {}
+
+ConsoleRegistrationMenu::~ConsoleRegistrationMenu() {}
 
 void ConsoleRegistrationMenu::RunRegistrationMenu() {
   RegistrationMessenger::ShowRegistrationLogo();
--- lib_cli/view_model/console_registration_menu.h
+++ lib_cli/view_model/console_registration_menu.h
@@ -1,9 +1,20 @@
 ﻿#ifndef CONSOLE_REGISTRATION_MENU_H
 #define CONSOLE_REGISTRATION_MENU_H
 
+#include <memory>
+
+class UserInputHandler;
+
 class ConsoleRegistrationMenu {
  public:
+  ConsoleRegistrationMenu();
+  ~ConsoleRegistrationMenu();
+
   void RunRegistrationMenu();
+
+ private:
+  std::unique_ptr<UserInputHandler> user_input_;
+  std::unique_ptr<UserInputHandler> menu_input_;
 };
 
 #endif  // CONSOLE_REGISTRATION_MENU_H
