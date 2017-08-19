0dbe383266035b7553d712ba76e4788e79bd2673
OrdinaryMind <1.ordinarymind@gmail.com>
2017-08-19 21:06:39 +0300

    Save commit

--- application/interface_presenter/authentication/registration_handler.cpp
+++ application/interface_presenter/authentication/registration_handler.cpp
@@ -1,7 +1,5 @@
 ﻿#include <registration_handler.h>
 
-#include <registration_status_messenger.h>
-
 void RegistrationHandler::HandleRegistrationData(const std::string login,
                                                  const std::string& password) {
   HandleLoginString(login);
@@ -17,37 +15,23 @@ void RegistrationHandler::HandleLoginString(const std::string& login) {
   login_status_ = registration_inspector_.InspectLoginString(login);
   switch (login_status_) {
     case ATM::RegistrationStatus::kCorrectLogin:
-      RegistrationStatusMessenger::CorrectLoginMessage();
       is_login_ok_ = true;
       break;
     case ATM::RegistrationStatus::kShortLogin:
-      RegistrationStatusMessenger::LoginLengthStatus(
-          RegistrationStatusMessenger::kShortLoginLength);
       break;
     case ATM::RegistrationStatus::kLongLogin:
-      RegistrationStatusMessenger::LoginLengthStatus(
-          RegistrationStatusMessenger::kLongLoginLength);
       break;
     case ATM::RegistrationStatus::kSpecialSymbols:
-      RegistrationStatusMessenger::LoginContainSpecialSymbol();
       break;
     case ATM::RegistrationStatus::kBeginWithSpace:
-      RegistrationStatusMessenger::LoginContainIncorrectSpacePosition(
-          RegistrationStatusMessenger::ContainSpaceSymbol::kBeginWithSpace);
       break;
     case ATM::RegistrationStatus::kEndWithSpace:
-      RegistrationStatusMessenger::LoginContainIncorrectSpacePosition(
-          RegistrationStatusMessenger::ContainSpaceSymbol::kEndWithSpace);
       break;
     case ATM::RegistrationStatus::kAdjacentSpaces:
-      RegistrationStatusMessenger::LoginContainIncorrectSpacePosition(
-          RegistrationStatusMessenger::ContainSpaceSymbol::kAdjecentSpaces);
       break;
     case ATM::RegistrationStatus::kContainOnlyDigits:
-      RegistrationStatusMessenger::LoginContainOnlyDigits();
       break;
     case ATM::RegistrationStatus::kEmptyString:
-      RegistrationStatusMessenger::LoginEmpty();
       break;
     default:
       break;
@@ -58,25 +42,17 @@ void RegistrationHandler::HandlePasswordString(const std::string& password) {
   password_status_ = registration_inspector_.InspectPasswordString(password);
   switch (password_status_) {
     case ATM::RegistrationStatus::kCorrectPassword:
-      RegistrationStatusMessenger::CorrectPasswordMessage();
       is_password_ok_ = true;
       break;
     case ATM::RegistrationStatus::kShortPassword:
-      RegistrationStatusMessenger::PasswordLength(
-          RegistrationStatusMessenger::kShortPasswordLength);
       break;
     case ATM::RegistrationStatus::kLongPassword:
-      RegistrationStatusMessenger::PasswordLength(
-          RegistrationStatusMessenger::kLongPasswordLength);
       break;
     case ATM::RegistrationStatus::kContainSpaceSymbol:
-      RegistrationStatusMessenger::PasswordContainSpaceSymbol();
       break;
     case ATM::kEmptyString:
-      RegistrationStatusMessenger::PasswordEmpty();
       break;
     default:
       break;
   }
-  RegistrationStatusMessenger::AddEmptyLines(2);
 }
