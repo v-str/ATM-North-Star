5df7d3234058a4254518d82a7290843688f37cea
OrdinaryMind <1.ordinarymind@gmail.com>
2017-08-19 18:40:12 +0300

    Add raw methods for registration handling

--- lib_atm_model/incoming_data_handling/atm_registration_handler.cpp
+++ lib_atm_model/incoming_data_handling/atm_registration_handler.cpp
@@ -1,1 +1,15 @@
 ﻿#include <atm_registration_handler.h>
+
+ATM::RegistrationStatus AtmRegistrationHandler::HandleLoginString(
+    const std::string login) {
+  ATM::RegistrationStatus login_status =
+      registration_inpector_.InspectLoginString(login);
+  return login_status;
+}
+
+ATM::RegistrationStatus AtmRegistrationHandler::HandlePasswordString(
+    const std::string password) {
+  ATM::RegistrationStatus password_status =
+      registration_inpector_.InspectPasswordString(password);
+  return password_status;
+}
--- lib_atm_model/incoming_data_handling/atm_registration_handler.h
+++ lib_atm_model/incoming_data_handling/atm_registration_handler.h
@@ -1,8 +1,21 @@
 ﻿#ifndef ATM_REGISTRATION_HANDLER_H
 #define ATM_REGISTRATION_HANDLER_H
 
+#include <string>
+
+#include <registration_inspector.h>
+#include <registration_status.h>
+
 class AtmRegistrationHandler {
  public:
+  ATM::RegistrationStatus HandleLoginString(const std::string login);
+  ATM::RegistrationStatus HandlePasswordString(const std::string password);
+
+ private:
+  RegistrationInspector registration_inpector_;
+
+  bool is_login_correct_ = false;
+  bool is_password_correct = false;
 };
 
 #endif  // ATM_REGISTRATION_HANDLER_H
