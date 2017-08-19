79f690e724c8459a9dec8077020f20bd786c23ba
OrdinaryMind <1.ordinarymind@gmail.com>
2017-08-19 18:24:26 +0300

    Add class for registration data handling
    Put it into atm model library for universal usage with different kinds
    of interface

---
+++ lib_atm_model/incoming_data_handling/atm_registration_handler.cpp
@@ -0,0 +1,1 @@
+﻿#include <atm_registration_handler.h>
---
+++ lib_atm_model/incoming_data_handling/atm_registration_handler.h
@@ -0,0 +1,8 @@
+﻿#ifndef ATM_REGISTRATION_HANDLER_H
+#define ATM_REGISTRATION_HANDLER_H
+
+class AtmRegistrationHandler {
+ public:
+};
+
+#endif  // ATM_REGISTRATION_HANDLER_H
