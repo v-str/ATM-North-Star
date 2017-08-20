5be7e9ca1ab364e029c8c4afe4c77514e85d7547
OrdinaryMind <1.ordinarymind@gmail.com>
2017-08-20 21:00:24 +0300

    Add registration status enumeration

---
+++ lib_cli/messengers/registration_messengers/registration_status.h
@@ -0,0 +1,22 @@
+﻿#ifndef REGISTRATION_STATUS_H
+#define REGISTRATION_STATUS_H
+
+namespace CONSOLE {
+enum RegistrationStatus {
+  kCorrectLogin,
+  kShortLogin,
+  kLongLogin,
+  kCorrectPassword,
+  kShortPassword,
+  kLongPassword,
+  kSpecialSymbols,
+  kBeginWithSpace,
+  kEndWithSpace,
+  kAdjacentSpaces,
+  kContainOnlyDigits,
+  kContainSpaceSymbol,
+  kEmptyString
+};
+}
+
+#endif  // REGISTRATION_STATUS_H
