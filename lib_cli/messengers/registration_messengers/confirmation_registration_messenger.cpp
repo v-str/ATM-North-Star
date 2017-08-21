d52c8cfc08834c451e28dde1620a8f40ce23a12d
OrdinaryMind <1.ordinarymind@gmail.com>
2017-08-21 23:35:52 +0300

    Add methods declarations for class

--- lib_cli/messengers/registration_messengers/confirmation_registration_messenger.cpp
+++ lib_cli/messengers/registration_messengers/confirmation_registration_messenger.cpp
@@ -1,1 +1,11 @@
 ﻿#include <confirmation_registration_messenger.h>
+
+#include <console_editor.h>
+
+int ConfirmationRegistrationMessenger::maximal_string_length_ = 0;
+
+void ConfirmationRegistrationMessenger::ShowConfirmationMessage(
+    const std::string& login_string, const std::string& password_string) {}
+
+void ConfirmationRegistrationMessenger::CalculateMaximalStringLength(
+    int login_string_length, int password_string_length) {}
--- lib_cli/messengers/registration_messengers/confirmation_registration_messenger.h
+++ lib_cli/messengers/registration_messengers/confirmation_registration_messenger.h
@@ -1,8 +1,18 @@
 ﻿#ifndef CONFIRMATION_REGISTRATION_MESSENGER_H
 #define CONFIRMATION_REGISTRATION_MESSENGER_H
 
+#include <string>
+
 class ConfirmationRegistrationMessenger {
  public:
+  static void ShowConfirmationMessage(const std::string& login_string,
+                                      const std::string& password_string);
+
+ private:
+  static void CalculateMaximalStringLength(int login_string_length,
+                                           int password_string_length);
+
+  static int maximal_string_length_;
 };
 
 #endif  // CONFIRMATION_REGISTRATION_MESSENGER_H
