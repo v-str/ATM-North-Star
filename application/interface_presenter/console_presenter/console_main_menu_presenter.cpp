26540c5c1c55df6ea93ac7b7688eced3e1d739af
OrdinaryMind <1.ordinarymind@gmail.com>
2017-08-21 22:25:10 +0300

    Add main menu presenter class

---
+++ application/interface_presenter/console_presenter/console_main_menu_presenter.cpp
@@ -0,0 +1,1 @@
+﻿#include <console_main_menu_presenter.h>
---
+++ application/interface_presenter/console_presenter/console_main_menu_presenter.h
@@ -0,0 +1,8 @@
+﻿#ifndef CONSOLE_MAIN_MENU_PRESENTER_H
+#define CONSOLE_MAIN_MENU_PRESENTER_H
+
+class ConsoleMainMenuPresenter {
+ public:
+};
+
+#endif  // CONSOLE_MAIN_MENU_PRESENTER_H
