4213f495885ceb0aa1b82bf5b0960a7b2aa86493
OrdinaryMind <1.ordinarymind@gmail.com>
2017-08-24 23:03:47 +0300

    Add method for frame setting

--- lib_cli/messengers/registration_messengers/confirmation_animation/confirmation_frame.cpp
+++ lib_cli/messengers/registration_messengers/confirmation_animation/confirmation_frame.cpp
@@ -1,1 +1,10 @@
 ﻿#include <confirmation_frame.h>
+
+std::string confirmation_frame::kText = "No text";
+std::string confirmation_frame::kFrameSymbol = "#";
+
+void confirmation_frame::SetFrame(const std::string& text,
+                                  const std::string& frame_symbol) {
+  kText = text;
+  kFrameSymbol = frame_symbol;
+}
--- lib_cli/messengers/registration_messengers/confirmation_animation/confirmation_frame.h
+++ lib_cli/messengers/registration_messengers/confirmation_animation/confirmation_frame.h
@@ -1,8 +1,16 @@
 ﻿#ifndef CONFIRMATION_FRAME_H
 #define CONFIRMATION_FRAME_H
 
+#include <string>
+
 class confirmation_frame {
  public:
+  static void SetFrame(const std::string& text,
+                       const std::string& frame_symbol);
+
+ private:
+  static std::string kText;
+  static std::string kFrameSymbol;
 };
 
 #endif  // CONFIRMATION_FRAME_H
