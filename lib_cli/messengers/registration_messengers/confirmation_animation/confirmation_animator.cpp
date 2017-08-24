ed7118f54db08907ab98080c9037657d6c09f942
OrdinaryMind <1.ordinarymind@gmail.com>
2017-08-24 23:08:29 +0300

    Remove deprecated code

--- lib_cli/messengers/registration_messengers/confirmation_animation/confirmation_animator.cpp
+++ lib_cli/messengers/registration_messengers/confirmation_animation/confirmation_animator.cpp
@@ -1,57 +1,15 @@
 ﻿#include <confirmation_animator.h>
 
+#include <confirmation_frame.h>
 #include <console_editor.h>
 #include <titler.h>
 
-std::string ConfirmationAnimator::kTitle = "ATM NORTH STAR";
-std::string ConfirmationAnimator::kGratingSymbol = "#";
-std::string ConfirmationAnimator::kConfirmationText = "CONFIRMATION ACCEPTED";
-std::string ConfirmationAnimator::kSpaceSymbol = " ";
-std::string ConfirmationAnimator::kLoginString = "";
-
 void ConfirmationAnimator::ShowConfirmationMessage(
     const std::string& login_string) {
-  kLoginString = login_string;
+  ConfirmationFrame::SetFrame(login_string, "#");
 
   Titler::WriteTitle("ATM \"NORTH STAR\"");
   DrawConfirmationFrame();
 }
 
-void ConfirmationAnimator::DrawConfirmationFrame() {
-  DrawHorizontalLine();
-  DrawEmptyLine();
-  DrawConfirmationTextLine();
-  DrawEmptyLine();
-}
-
-void ConfirmationAnimator::DrawHorizontalLine() {
-  for (int i = 0; i < kLineLength; ++i) {
-    ConsoleEditor::WriteText(kGratingSymbol);
-  }
-}
-
-void ConfirmationAnimator::DrawEmptyLine() {
-  std::string empty_space;
-  for (int i = 0; i < kLineLength - 2; ++i) {
-    empty_space += kSpaceSymbol;
-  }
-
-  ConsoleEditor::AddEmptyLineNTimes(1);
-  ConsoleEditor::WriteText(kGratingSymbol + empty_space + kGratingSymbol);
-}
-
-void ConfirmationAnimator::DrawConfirmationTextLine() {
-  std::string empty_space = "         ";
-
-  std::string final_confirmation_string = kGratingSymbol + empty_space +
-                                          kConfirmationText + empty_space +
-                                          kGratingSymbol;
-
-  ConsoleEditor::AddEmptyLineNTimes(1);
-  ConsoleEditor::WriteText(final_confirmation_string);
-}
-
-void ConfirmationAnimator::DrawLoginStringLine() {
-  int free_line_space =
-      kLineLength - (kGratingSymbol.length() * 2) - kLoginString.length();
-}
+void ConfirmationAnimator::DrawConfirmationFrame() {}
--- lib_cli/messengers/registration_messengers/confirmation_animation/confirmation_animator.h
+++ lib_cli/messengers/registration_messengers/confirmation_animation/confirmation_animator.h
@@ -9,19 +9,6 @@ class ConfirmationAnimator {
 
  private:
   static void DrawConfirmationFrame();
-
-  static void DrawHorizontalLine();
-  static void DrawEmptyLine();
-  static void DrawConfirmationTextLine();
-  static void DrawLoginStringLine();
-
-  static std::string kTitle;
-  static std::string kGratingSymbol;
-  static std::string kSpaceSymbol;
-  static std::string kConfirmationText;
-  static std::string kLoginString;
-
-  static const int kLineLength = 41;
 };
 
 #endif  // CONFIRMATION_ANIMATOR_H
