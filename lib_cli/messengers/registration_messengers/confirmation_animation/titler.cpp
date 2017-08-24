19cf5c331f5e2736faaced04f0d9d687b81879ca
OrdinaryMind <1.ordinarymind@gmail.com>
2017-08-24 22:53:19 +0300

    Add string for indent storing

--- lib_cli/messengers/registration_messengers/confirmation_animation/titler.cpp
+++ lib_cli/messengers/registration_messengers/confirmation_animation/titler.cpp
@@ -2,9 +2,11 @@
 
 #include <console_editor.h>
 
+std::string Titler::kIndent = "\t    ";
+
 void Titler::WriteTitle(const std::string title_string) {
   ConsoleEditor::AddEmptyLineNTimes(1);
-  ConsoleEditor::WriteText("\t\t");
+  ConsoleEditor::WriteText(kIndent);
   ConsoleEditor::WriteText(title_string);
   ConsoleEditor::AddEmptyLineNTimes(2);
 }
--- lib_cli/messengers/registration_messengers/confirmation_animation/titler.h
+++ lib_cli/messengers/registration_messengers/confirmation_animation/titler.h
@@ -6,6 +6,9 @@
 class Titler {
  public:
   static void WriteTitle(const std::string title_string);
+
+ private:
+  static std::string kIndent;
 };
 
 #endif  // TITLER_H
