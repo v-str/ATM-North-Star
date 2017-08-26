edf9017e17c283bdf7abbe2173223f330bb1a6af
OrdinaryMind <1.ordinarymind@gmail.com>
2017-08-26 20:06:36 +0300

    Add main menu presentation method

--- lib_cli/view_model/console_main_menu.cpp
+++ lib_cli/view_model/console_main_menu.cpp
@@ -1,1 +1,5 @@
 ﻿#include <console_main_menu.h>
+
+#include <main_menu_messenger.h>
+
+void ConsoleMainMenu::RunMainMenu() { MainMenuMessenger::ShowMainMenu(); }
--- lib_cli/view_model/console_main_menu.h
+++ lib_cli/view_model/console_main_menu.h
@@ -1,8 +1,16 @@
 ﻿#ifndef CONSOLE_MAIN_MENU_H
 #define CONSOLE_MAIN_MENU_H
 
+#include <memory>
+
+#include <user_input.h>
+
 class ConsoleMainMenu {
  public:
+  void RunMainMenu();
+
+ private:
+  std::unique_ptr<UserInput> user_input_;
 };
 
 #endif  // CONSOLE_MAIN_MENU_H
