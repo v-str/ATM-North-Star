f5e1ebceaad5710dbcb96ec59ae633aa1df027cd
OrdinaryMind <1.ordinarymind@gmail.com>
2017-09-02 15:18:07 +0300

    Add container for reports storing

--- lib_atm_model/interface_presenter/process_reporter.h
+++ lib_atm_model/interface_presenter/process_reporter.h
@@ -1,8 +1,13 @@
 ﻿#ifndef PROCESS_REPORTER_H
 #define PROCESS_REPORTER_H
 
+#include <string>
+#include <vector>
+
 class ProcessReporter {
  public:
+ private:
+  std::vector<std::string> process_report_;
 };
 
 #endif  // PROCESS_REPORTER_H
