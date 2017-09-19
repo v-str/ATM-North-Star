56161d55a2540548f8ff262b1c0385631c386495
OrdinaryMind <1.ordinarymind@gmail.com>
2017-09-19 16:41:33 +0300

    Add method for filled line drawing

--- lib_cli/messengers/statement_messenger.cpp
+++ lib_cli/messengers/statement_messenger.cpp
@@ -6,22 +6,22 @@
 
 void StatementMessenger::ShowStatement(double amount_of_cash) {
   ConsoleEditor::ClearScreen();
-  ConsoleEditor::WriteText(
-      "\t##################\n"
-      "\t#                #\n"
-      "\t#  28 Green st.  #\n"
-      "\t#   New - York   #\n"
-      "\t#                #\n"
-      "\t#   NORTH BANK   #\n"
-      "\t#                #\n"
-      "\t# Balance:       #\n"
-      "\t#  $ " +
-      std::to_string(amount_of_cash) + "spaces" + "#\n" +
-      "\t#                #\n"
-      "\t#  HAVE A NICE   #\n"
-      "\t#      DAY       #\n"
-      "\t#                #\n"
-      "\t##################\n");
+  //  ConsoleEditor::WriteText(
+  //      "\t##################\n"
+  //      "\t#                #\n"
+  //      "\t#  28 Green st.  #\n"
+  //      "\t#   New - York   #\n"
+  //      "\t#                #\n"
+  //      "\t#   NORTH BANK   #\n"
+  //      "\t#                #\n"
+  //      "\t# Balance:       #\n"
+  //      "\t#  $ " +
+  //      std::to_string(amount_of_cash) + "spaces" + "#\n" +
+  //      "\t#                #\n"
+  //      "\t#  HAVE A NICE   #\n"
+  //      "\t#      DAY       #\n"
+  //      "\t#                #\n"
+  //      "\t##################\n");
 
   ConsoleEditor::WriteTextWithDelayPerSymbol("\n\t# 1. Main menu");
   ConsoleEditor::WriteTextWithDelayPerSymbol("\n\t# 2. Quit\n\t");
@@ -29,9 +29,17 @@ void StatementMessenger::ShowStatement(double amount_of_cash) {
   ConsoleEditor::IgnoreCinLine();
 }
 
+void StatementMessenger::DrawFilledLine() {
+  std::string filled_line;
+  for (int i = 0; i < kFilledLineLength; ++i) {
+    filled_line.append("#");
+  }
+  ConsoleEditor::WriteText(filled_line);
+}
+
 void StatementMessenger::DrawEmptyLine() {
   std::string empty_line = "#";
-  for (int i = 0; i < kLineLength; ++i) {
+  for (int i = 0; i < kEmptyLineLength; ++i) {
     empty_line.append(" ");
   }
   empty_line.append("#");
