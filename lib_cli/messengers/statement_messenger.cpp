9e3196f1f9db617eab65d8ee6e549f325b5dd2d2
OrdinaryMind <1.ordinarymind@gmail.com>
2017-09-19 18:47:49 +0300

    Add method for text line drawing

--- lib_cli/messengers/statement_messenger.cpp
+++ lib_cli/messengers/statement_messenger.cpp
@@ -1,27 +1,20 @@
 ﻿#include <statement_messenger.h>
 
-#include <string>
-
 #include <console_editor.h>
 
 void StatementMessenger::ShowStatement(double amount_of_cash) {
   ConsoleEditor::ClearScreen();
-  //  ConsoleEditor::WriteText(
-  //      "\t##################\n"
-  //      "\t#                #\n"
-  //      "\t#  28 Green st.  #\n"
-  //      "\t#   New - York   #\n"
-  //      "\t#                #\n"
-  //      "\t#   NORTH BANK   #\n"
-  //      "\t#                #\n"
-  //      "\t# Balance:       #\n"
-  //      "\t#  $ " +
-  //      std::to_string(amount_of_cash) + "spaces" + "#\n" +
-  //      "\t#                #\n"
-  //      "\t#  HAVE A NICE   #\n"
-  //      "\t#      DAY       #\n"
-  //      "\t#                #\n"
-  //      "\t##################\n");
+
+  DrawFilledLine();
+  DrawEmptyLine();
+  DrawEmptyLine();
+  DrawEmptyLine();
+  DrawEmptyLine();
+  DrawTextLine("hell");
+  DrawEmptyLine();
+  DrawEmptyLine();
+  DrawEmptyLine();
+  DrawFilledLine();
 
   ConsoleEditor::WriteTextWithDelayPerSymbol("\n\t# 1. Main menu");
   ConsoleEditor::WriteTextWithDelayPerSymbol("\n\t# 2. Quit\n\t");
@@ -29,20 +22,38 @@ void StatementMessenger::ShowStatement(double amount_of_cash) {
   ConsoleEditor::IgnoreCinLine();
 }
 
+void StatementMessenger::DrawTextLine(const std::string& text) {
+  int work_line_space = kFilledLineLength - 2;
+  int free_line_space = work_line_space - text.length();
+  std::string text_line = "\t#";
+  if ((free_line_space % 2) == 0) {
+    for (int i = 0; i < free_line_space / 2; ++i) {
+      text_line.append(" ");
+    }
+    text_line += text;
+    for (int i = 0; i < free_line_space / 2; ++i) {
+      text_line.append(" ");
+    }
+    text_line.append("#\n");
+  }
+  ConsoleEditor::WriteText(text_line);
+}
+
 void StatementMessenger::DrawFilledLine() {
-  std::string filled_line;
+  std::string filled_line = "\t";
   for (int i = 0; i < kFilledLineLength; ++i) {
     filled_line.append("#");
   }
+  filled_line.append("\n");
   ConsoleEditor::WriteText(filled_line);
 }
 
 void StatementMessenger::DrawEmptyLine() {
-  std::string empty_line = "#";
+  std::string empty_line = "\t#";
   for (int i = 0; i < kEmptyLineLength; ++i) {
     empty_line.append(" ");
   }
-  empty_line.append("#");
+  empty_line.append("#\n");
 
   ConsoleEditor::WriteText(empty_line);
 }
--- lib_cli/messengers/statement_messenger.h
+++ lib_cli/messengers/statement_messenger.h
@@ -1,14 +1,19 @@
 ﻿#ifndef STATEMENT_MESSENGER_H
 #define STATEMENT_MESSENGER_H
 
+#include <string>
+
 class StatementMessenger {
  public:
   static void ShowStatement(double amount_of_cash);
 
  private:
+  static void DrawTextLine(const std::string &text);
+  static void DrawFilledLine();
   static void DrawEmptyLine();
 
-  static const int kLineLength = 18;
+  static const int kFilledLineLength = 20;
+  static const int kEmptyLineLength = 18;
 };
 
 #endif  // STATEMENT_MESSENGER_H
