1fb0c0f2d2e7a99a0bf4b6aa93642dbbdc571f47
OrdinaryMind <1.ordinarymind@gmail.com>
2017-09-21 15:48:19 +0300

    Add return values in methods

--- lib_cli/view_model/credit_menu.cpp
+++ lib_cli/view_model/credit_menu.cpp
@@ -2,12 +2,19 @@
 
 #include <credit_messenger.h>
 
-void CreditMenu::DisplayCreditMenu() { CreditMessenger::DisplayCreditMenu(); }
+void CreditMenu::DisplayCreditMenu() {
+  CreditMessenger::DisplayCreditMenu();
+  ProcessUserInput();
+}
 
-bool CreditMenu::IsUserWantCredit() const {}
+bool CreditMenu::IsUserWantCredit() const { return user_want_credit_; }
 
-bool CreditMenu::IsUserWantCreditCalculator() const {}
+bool CreditMenu::IsUserWantCreditCalculator() const {
+  return user_want_credit_calculator_;
+}
 
-bool CreditMenu::IsUserWantMainMenu() const {}
+bool CreditMenu::IsUserWantMainMenu() const { return user_want_main_menu_; }
 
-bool CreditMenu::IsUserWantQuit() const {}
+bool CreditMenu::IsUserWantQuit() const { return user_want_quit_; }
+
+void CreditMenu::ProcessUserInput() {}
--- lib_cli/view_model/credit_menu.h
+++ lib_cli/view_model/credit_menu.h
@@ -9,6 +9,14 @@ class CreditMenu {
   bool IsUserWantCreditCalculator() const;
   bool IsUserWantMainMenu() const;
   bool IsUserWantQuit() const;
+
+ private:
+  void ProcessUserInput();
+
+  bool user_want_credit_ = false;
+  bool user_want_credit_calculator_ = false;
+  bool user_want_main_menu_ = false;
+  bool user_want_quit_ = false;
 };
 
 #endif  // CREDIT_MENU_H
