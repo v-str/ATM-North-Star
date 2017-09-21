9c0af17c1dd0af7174035fc02bb71dc0af06ff2c
OrdinaryMind <1.ordinarymind@gmail.com>
2017-09-21 18:15:28 +0300

    Rename class

--- lib_cli/view_model/credit_calculator.h
+++
@@ -1,9 +0,0 @@
-﻿#ifndef CREDIT_CALCULATOR_H
-#define CREDIT_CALCULATOR_H
-
-class CreditCalculator {
- public:
-
-};
-
-#endif  // CREDIT_CALCULATOR_H
---
+++ lib_cli/view_model/credit_calculator_menu.h
@@ -0,0 +1,8 @@
+﻿#ifndef CREDIT_CALCULATOR_MENU_H
+#define CREDIT_CALCULATOR_MENU_H
+
+class CreditCalculatorMenu {
+ public:
+};
+
+#endif  // CREDIT_CALCULATOR_MENU_H
