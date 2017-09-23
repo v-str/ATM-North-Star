1bd884f9845020158ae8068e1c889fd6f233be62
OrdinaryMind <1.ordinarymind@gmail.com>
2017-09-23 18:09:40 +0300

    Add method for credit calculator data handling

--- lib_atm_model/credit_calculator/calculator_data_handler.cpp
+++ lib_atm_model/credit_calculator/calculator_data_handler.cpp
@@ -1,1 +1,5 @@
 ﻿#include <calculator_data_handler.h>
+
+CalculatorDataHandler::HandleData(const std::string credit_sum,
+                                  const std::string credit_interest_rate,
+                                  const std::string amount_of_credit_months) {}
--- lib_atm_model/credit_calculator/calculator_data_handler.h
+++ lib_atm_model/credit_calculator/calculator_data_handler.h
@@ -1,8 +1,22 @@
 ﻿#ifndef CALCULATOR_DATA_HANDLER_H
 #define CALCULATOR_DATA_HANDLER_H
 
-class CalculatorDataChecker {
+#include <string>
+
+#include <credit_data_bounder.h>
+
+class CalculatorDataHandler {
  public:
+  HandleData(const std::string credit_sum,
+             const std::string credit_interest_rate,
+             const std::string amount_of_credit_months);
+
+ private:
+  CreditDataBounder credit_data_bounder_;
+
+  int credit_sum_;
+  double interest_rate_;
+  int amount_of_credit_months_;
 };
 
 #endif  // CALCULATOR_DATA_HANDLER_H
