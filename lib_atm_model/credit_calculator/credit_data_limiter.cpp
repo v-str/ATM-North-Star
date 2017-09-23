e603fd9e61836486e8f690096613a448042de865
OrdinaryMind <1.ordinarymind@gmail.com>
2017-09-23 16:07:26 +0300

    Add constructor for credit data limitation

--- lib_atm_model/credit_calculator/credit_data_limiter.cpp
+++ lib_atm_model/credit_calculator/credit_data_limiter.cpp
@@ -1,1 +1,7 @@
 ﻿#include <credit_data_limiter.h>
+
+CreditDataLimiter::CreditDataLimiter() {
+  credit_sum_limit_ = std::make_pair<int, int>(10000, 500000);
+  interest_rate_limit_ = std::make_pair<double, double>(5.0, 50.0);
+  amount_of_months_limit_ = std::make_pair<int, int>(1, 60);
+}
--- lib_atm_model/credit_calculator/credit_data_limiter.h
+++ lib_atm_model/credit_calculator/credit_data_limiter.h
@@ -1,9 +1,15 @@
 ﻿#ifndef CREDIT_DATA_LIMITER_H
 #define CREDIT_DATA_LIMITER_H
 
+#include <utility>
+
 class CreditDataLimiter {
  public:
-
+    CreditDataLimiter();
+ private:
+   std::pair<int, int> credit_sum_limit_;
+   std::pair<double, double> interest_rate_limit_;
+   std::pair<int, int> amount_of_months_limit_;
 };
 
 #endif  // CREDIT_DATA_LIMITER_H
