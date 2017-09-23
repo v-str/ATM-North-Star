2ba7de10cd948c8fb4eface69e01f9545b6e73ff
OrdinaryMind <1.ordinarymind@gmail.com>
2017-09-23 16:20:29 +0300

    Add setters for credit limiter class

--- lib_atm_model/credit_calculator/credit_data_limiter.cpp
+++ lib_atm_model/credit_calculator/credit_data_limiter.cpp
@@ -1,7 +1,27 @@
 ﻿#include <credit_data_limiter.h>
 
-CreditDataLimiter::CreditDataLimiter() {
-  credit_sum_limit_ = std::make_pair<int, int>(10000, 500000);
-  interest_rate_limit_ = std::make_pair<double, double>(5.0, 50.0);
-  amount_of_months_limit_ = std::make_pair<int, int>(1, 60);
+CreditDataLimiter::CreditDataLimiter()
+    : lower_credit_sum_bound_(10000),
+      upper_credit_sum_bound_(500000),
+      lower_interest_rate_bound_(5.0),
+      upper_interest_rate_bound_(60.0),
+      lower_amount_of_month_bound_(1),
+      upper_ampunt_of_month_bound_(60) {}
+
+void CreditDataLimiter::SetCreditSumBound(int lower_credit_sum_bound,
+                                          int upper_credit_sum_bound) {
+  lower_credit_sum_bound_ = lower_credit_sum_bound;
+  upper_credit_sum_bound_ = upper_credit_sum_bound;
+}
+
+void CreditDataLimiter::SetIntersetRateBound(double lower_interest_rate_bound,
+                                             double upper_interest_rate_bound) {
+  lower_interest_rate_bound_ = lower_interest_rate_bound;
+  upper_interest_rate_bound_ = upper_interest_rate_bound;
+}
+
+void CreditDataLimiter::SetAmountOfMonthBound(
+    double lower_ampunt_of_month_bound, double upper_ampunt_of_month_bound) {
+  lower_amount_of_month_bound_ = lower_ampunt_of_month_bound;
+  upper_ampunt_of_month_bound_ = upper_ampunt_of_month_bound;
 }
--- lib_atm_model/credit_calculator/credit_data_limiter.h
+++ lib_atm_model/credit_calculator/credit_data_limiter.h
@@ -1,15 +1,24 @@
 ﻿#ifndef CREDIT_DATA_LIMITER_H
 #define CREDIT_DATA_LIMITER_H
 
-#include <utility>
-
 class CreditDataLimiter {
  public:
-    CreditDataLimiter();
+  CreditDataLimiter();
+
+  void SetCreditSumBound(int lower_credit_sum_bound,
+                         int upper_credit_sum_bound);
+  void SetIntersetRateBound(double lower_interest_rate_bound,
+                            double upper_interest_rate_bound);
+  void SetAmountOfMonthBound(double lower_ampunt_of_month_bound,
+                             double upper_ampunt_of_month_bound);
+
  private:
-   std::pair<int, int> credit_sum_limit_;
-   std::pair<double, double> interest_rate_limit_;
-   std::pair<int, int> amount_of_months_limit_;
+  int lower_credit_sum_bound_;
+  int upper_credit_sum_bound_;
+  double lower_interest_rate_bound_;
+  double upper_interest_rate_bound_;
+  int lower_amount_of_month_bound_;
+  int upper_ampunt_of_month_bound_;
 };
 
 #endif  // CREDIT_DATA_LIMITER_H
