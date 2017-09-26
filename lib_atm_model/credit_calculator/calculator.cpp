a14e5e9a554fb97b8aecc67e425e55695524908d
OrdinaryMind <1.ordinarymind@gmail.com>
2017-09-26 14:24:47 +0300

    Pass parameters to calculator

--- lib_atm_model/credit_calculator/calculator.cpp
+++ lib_atm_model/credit_calculator/calculator.cpp
@@ -1,2 +1,11 @@
 ﻿#include <calculator.h>
 
+Calculator::Calculator()
+    : credit_sum_(0), credit_interest_rate_(0.0), amount_of_months_(0) {}
+
+void Calculator::CalculateCredit(int credit_sum, double credit_interest_rate,
+                                 int amount_of_months) {
+  credit_sum_ = credit_sum;
+  credit_interest_rate_ = credit_interest_rate;
+  amount_of_months_ = amount_of_months;
+}
--- lib_atm_model/credit_calculator/calculator.h
+++ lib_atm_model/credit_calculator/calculator.h
@@ -3,7 +3,15 @@
 
 class Calculator {
  public:
+  Calculator();
 
+  void CalculateCredit(int credit_sum, double credit_interest_rate,
+                       int amount_of_months);
+
+ private:
+  int credit_sum_;
+  double credit_interest_rate_;
+  int amount_of_months_;
 };
 
 #endif  // CALCULATOR_H
