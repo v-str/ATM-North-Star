c6e39818d094917269de28be423fc8c40bb2f3fc
OrdinaryMind <1.ordinarymind@gmail.com>
2017-09-26 14:39:35 +0300

    Add variables to constructor

--- lib_atm_model/credit_calculator/calculator.cpp
+++ lib_atm_model/credit_calculator/calculator.cpp
@@ -1,7 +1,11 @@
 ﻿#include <calculator.h>
 
 Calculator::Calculator()
-    : credit_sum_(0), credit_interest_rate_(0.0), amount_of_months_(0) {}
+    : credit_sum_(0),
+      credit_interest_rate_(0.0),
+      amount_of_months_(0),
+      mounthly_payment_(0.0),
+      maturiry_in_years_(0.0) {}
 
 void Calculator::CalculateCredit(int credit_sum, double credit_interest_rate,
                                  int amount_of_months) {
@@ -9,3 +13,5 @@ void Calculator::CalculateCredit(int credit_sum, double credit_interest_rate,
   credit_interest_rate_ = credit_interest_rate;
   amount_of_months_ = amount_of_months;
 }
+
+void Calculator::CalculateMonthlyPayment() {}
--- lib_atm_model/credit_calculator/calculator.h
+++ lib_atm_model/credit_calculator/calculator.h
@@ -9,9 +9,16 @@ class Calculator {
                        int amount_of_months);
 
  private:
+  void CalculateMonthlyPayment();
+
   int credit_sum_;
   double credit_interest_rate_;
   int amount_of_months_;
+
+  double mounthly_payment_;
+  double maturiry_in_years_;
+
+  static const int kMonthsPerYear = 12;
 };
 
 #endif  // CALCULATOR_H
