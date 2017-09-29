26cc55b659d40967d1f0012b7026e78adcfffaea
OrdinaryMind <1.ordinarymind@gmail.com>
2017-09-29 23:26:43 +0300

    Test credit payments

--- lib_atm_model/credit_calculator/credit_table.cpp
+++ lib_atm_model/credit_calculator/credit_table.cpp
@@ -3,14 +3,15 @@
 #include <iostream>
 
 CreditTable::CreditTable()
-    : credit_sum_(0),
+    : credit_sum_(0.0),
       monthly_payment_(0.0),
       percentage_coefficient_(0.0),
       credit_term_months_(0),
-      percentages_payment_per_month_(0.0),
-      payment_per_month_(0.0) {}
+      interest_charge_(0.0),
+      main_debp_payment_(0.0) {}
 
-void CreditTable::CalculateCreditTable(int credit_sum, double monthly_payment,
+void CreditTable::CalculateCreditTable(int credit_sum,
+                                       double monthly_payment,
                                        double percentage_coefficient,
                                        int credit_term_months) {
   credit_sum_ = credit_sum;
@@ -22,23 +23,21 @@ void CreditTable::CalculateCreditTable(int credit_sum, double monthly_payment,
 }
 
 void CreditTable::ConstructCreditTable() {
-  std::cout
-      << "\n\t#  |  Credit dept | Percent payment | Monthly payment | Total\n";
-
-  double credit_dept = 0.0;
-  int count = 0;
-
   while (credit_sum_ >= monthly_payment_) {
-    percentages_payment_per_month_ =
-        credit_sum_ * (percentage_coefficient_ / kMonthsPerYear);
-    payment_per_month_ = monthly_payment_ - percentages_payment_per_month_;
-    credit_dept = credit_sum_ - payment_per_month_;
-    credit_sum_ = credit_dept;
-    count++;
+    interest_charge_ = credit_sum_ * (percentage_coefficient_ / kMonthsPerYear);
+    main_debp_payment_ = monthly_payment_ - interest_charge_;
+    credit_sum_ -= main_debp_payment_;
 
-    std::cout << "\t" << count << "   " << credit_dept << "        "
-              << percentages_payment_per_month_ << "          "
-              << payment_per_month_ << "        "
-              << percentages_payment_per_month_ + payment_per_month_ << "\n";
+    interest_charges_.push_back(interest_charge_);
+    balance_owed_.push_back(credit_sum_);
+    main_debt_payments_.push_back(main_debp_payment_);
   }
+  double rest_of_interest_charge = monthly_payment_ - credit_sum_;
+
+  main_debp_payment_ = monthly_payment_ - rest_of_interest_charge;
+  credit_sum_ -= main_debp_payment_;
+
+  interest_charges_.push_back(rest_of_interest_charge);
+  balance_owed_.push_back(credit_sum_);
+  main_debt_payments_.push_back(main_debp_payment_);
 }
--- lib_atm_model/credit_calculator/credit_table.h
+++ lib_atm_model/credit_calculator/credit_table.h
@@ -7,24 +7,25 @@ class CreditTable {
  public:
   CreditTable();
 
-  void CalculateCreditTable(int credit_sum, double monthly_payment,
+  void CalculateCreditTable(int credit_sum,
+                            double monthly_payment,
                             double percentage_coefficient,
                             int credit_term_months);
 
  private:
   void ConstructCreditTable();
 
-  int credit_sum_;
+  double credit_sum_;
   double monthly_payment_;
   double percentage_coefficient_;
   int credit_term_months_;
 
-  double percentages_payment_per_month_;
-  double payment_per_month_;
+  double interest_charge_;
+  double main_debp_payment_;
 
   std::vector<double> balance_owed_;
-  std::vector<double> percentages_debt_;
-  std::vector<double> principal_debt_;
+  std::vector<double> interest_charges_;
+  std::vector<double> main_debt_payments_;
 
   static const int kMonthsPerYear = 12;
 };
