3835d10db3f6a6dcbb2f46486d22d2b5176dfab2
OrdinaryMind <1.ordinarymind@gmail.com>
2017-09-30 01:32:15 +0300

    Add method for align string returning

--- lib_cli/messengers/credit_messengers/credit_payments_table.cpp
+++ lib_cli/messengers/credit_messengers/credit_payments_table.cpp
@@ -3,17 +3,29 @@
 #include <iomanip>
 #include <sstream>
 
+#include <console_editor.h>
+
 void CreditPaymentsTable::BuildCreditTable(
+    double monthly_payment,
+    int amount_of_months,
     const std::vector<double>& sum_of_owed_credit,
     const std::vector<double>& sum_of_interest_charges,
-    const std::vector<double>& sum_of_main_debt_payment,
-    double monthly_payment,
-    int amount_of_months) {
+    const std::vector<double>& sum_of_main_debt_payment) {
   for (int i = 1; i <= amount_of_months; ++i) {
   }
 }
 
-std::string CreditPaymentsTable::AlignCell(const std::string& string_content) {}
+std::string CreditPaymentsTable::AlignCell(int cell_size,
+                                           const std::string& string_content) {
+  int string_content_size = string_content.size();
+  int spaces_before_string = cell_size - string_content_size - 1;
+
+  std::string aligned_string(spaces_before_string, ' ');
+  aligned_string += string_content;
+  aligned_string += " ";
+
+  return aligned_string;
+}
 
 std::string CreditPaymentsTable::ConvertToString(double value) {
   std::stringstream conversion_stream;
--- lib_cli/messengers/credit_messengers/credit_payments_table.h
+++ lib_cli/messengers/credit_messengers/credit_payments_table.h
@@ -6,17 +6,17 @@
 
 class CreditPaymentsTable {
  public:
-  void BuildCreditTable(const std::vector<double>& sum_of_owed_credit,
+  void BuildCreditTable(double monthly_payment,
+                        int amount_of_months,
+                        const std::vector<double>& sum_of_owed_credit,
                         const std::vector<double>& sum_of_interest_charges,
-                        const std::vector<double>& sum_of_main_debt_payment,
-                        double monthly_payment,
-                        int amount_of_months);
+                        const std::vector<double>& sum_of_main_debt_payment);
 
  private:
-  std::string AlignCell(const std::string& string_content);
+  std::string AlignCell(int cell_size, const std::string& string_content);
   std::string ConvertToString(double value);
 
-  static const int kCellMargin = 12;
+  static const int kCellSize = 15;
 };
 
 #endif  // CREDIT_PAYMENTS_TABLE_H
