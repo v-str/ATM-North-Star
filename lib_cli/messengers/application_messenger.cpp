3525a737adda408e7cbc34134077b55feeb31fd6
OrdinaryMind <1.ordinarymind@gmail.com>
2017-10-02 20:34:51 +0300

    Save commit

---
+++ lib_cli/messengers/application_messenger.cpp
@@ -0,0 +1,15 @@
+﻿#include <application_messenger.h>
+
+#include <console_editor.h>
+
+void ApplicationMessenger::ShowIncorrectMenuInput() {
+  ConsoleEditor::WriteTextWithDelayPerSymbol(
+      "\t# Incorrect input, please try again", 10);
+  ConsoleEditor::WriteTextWithDelayPerSymbol("...", 70);
+  ConsoleEditor::WriteTextWithDelayPerSymbol("\n\t# Enter: ");
+}
+
+void ApplicationMessenger::PressEnterToContinue() {
+  ConsoleEditor::WriteText("\n\t## Press \"Enter\" to continue...");
+  ConsoleEditor::IgnoreCinLine();
+}
---
+++ lib_cli/messengers/application_messenger.h
@@ -0,0 +1,10 @@
+﻿#ifndef APPLICATION_MESSENGER_H
+#define APPLICATION_MESSENGER_H
+
+class ApplicationMessenger {
+ public:
+  static void ShowIncorrectMenuInput();
+  static void PressEnterToContinue();
+};
+
+#endif  // APPLICATION_MESSENGER_H
