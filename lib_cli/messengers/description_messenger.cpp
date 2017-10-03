0144c28fcbd38dba4c32c1598fd190ea3bd8dd1b
OrdinaryMind <1.ordinarymind@gmail.com>
2017-10-03 23:33:19 +0300

    Add frame for ATM description

--- lib_cli/messengers/description_messenger.cpp
+++ lib_cli/messengers/description_messenger.cpp
@@ -6,10 +6,14 @@
 void DescriptionMessenger::ShowDescription() {
   ConsoleEditor::ClearScreen();
   ConsoleEditor::WriteText(
-      "ATM North star is simple prototype of ATM,\n"
-      "which maintain basic ATM-operations like a\n"
-      "refill and cash withdrawal.\n"
-      "Also it provide annuity payment calculator function.\n"
-      "It intended for Windows and Unix-based operation systems.\n\n");
+      "#############################################################\n"
+      "#                                                           #\n"
+      "# ATM North star is simple prototype of ATM,                #\n"
+      "# which maintain basic ATM-operations like a                #\n"
+      "# refill and cash withdrawal.                               #\n"
+      "# Also it provide annuity payment calculator function.      #\n"
+      "# It intended for Windows and Unix-based operation systems. #\n"
+      "#                                                           #\n"
+      "#############################################################\n\n");
   ApplicationMessenger::PressEnterToContinue();
 }
