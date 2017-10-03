b6134131a0bd16eefeb2b5bfa0d973a9e864d83a
OrdinaryMind <1.ordinarymind@gmail.com>
2017-10-03 13:47:06 +0300

    Rename enumaration

--- application/application_launch/main.cpp
+++ application/application_launch/main.cpp
@@ -2,16 +2,15 @@
 #include <mode_configurator.h>
 
 int main(int argc, char* argv[]) {
-  enum ProgramInterface { kConsoleInterface, kGraphicalInterface };
+  enum Mode { CLI, GUI };
 
   ModeConfigurator mode_configurator;
-  Launcher launcher;
-
   int mode = mode_configurator.GetUIMode();
 
-  if (mode == kConsoleInterface) {
+  Launcher launcher;
+  if (mode == CLI) {
     launcher.LaunchConsoleMode();
-  } else if (mode == kGraphicalInterface) {
+  } else if (mode == GUI) {
     launcher.LaunchGuiMode(argc, argv);
   } else {
     launcher.DisplayErrorReport();
