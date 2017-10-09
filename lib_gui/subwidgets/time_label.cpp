d411df55037a05a9d47e10fa3a2ea8fa8cae90e0
OrdinaryMind <1.ordinarymind@gmail.com>
2017-10-09 15:14:18 +0300

    Extract default time label settings Add preparation code for arrangement compositor

--- lib_gui/subwidgets/time_label.cpp
+++ lib_gui/subwidgets/time_label.cpp
@@ -2,15 +2,28 @@
 
 #include <QTimer>
 
+#include <geometry_composer.h>
+#include <side.h>
 #include <timedate_changer.h>
 
-TimeLabel::TimeLabel(QLabel *parent)
-    : QLabel(parent), timer_(new QTimer(this)) {
+TimeLabel::TimeLabel(QLabel* parent)
+    : QLabel(parent),
+      timer_(new QTimer(this)),
+      composer_(new GeometryComposer) {
+  SetDefaultSettings();
+}
+
+TimeLabel::~TimeLabel() {
+  delete timer_;
+  delete composer_;
+}
+
+void TimeLabel::UpdateTime() { TimeDateChanger::ChangeTime(this); }
+
+void TimeLabel::resizeEvent(QResizeEvent* event) {}
+
+void TimeLabel::SetDefaultSettings() {
   setAlignment(Qt::AlignCenter);
   timer_->start(kOneSecond);
   connect(timer_, SIGNAL(timeout()), SLOT(UpdateTime()));
 }
-
-TimeLabel::~TimeLabel() { delete timer_; }
-
-void TimeLabel::UpdateTime() { TimeDateChanger::ChangeTime(this); }
--- lib_gui/subwidgets/time_label.h
+++ lib_gui/subwidgets/time_label.h
@@ -4,6 +4,7 @@
 #include <QLabel>
 
 class QTimer;
+class GeometryComposer;
 
 class TimeLabel : public QLabel {
   Q_OBJECT
@@ -11,13 +12,21 @@ class TimeLabel : public QLabel {
   TimeLabel(QLabel* parent = nullptr);
   ~TimeLabel();
 
-public slots:
+ public slots:
   void UpdateTime();
+
+ protected:
+  void resizeEvent(QResizeEvent* event);
 
  private:
   void SetConnection();
+  void SetDefaultSettings();
 
   QTimer* timer_ = nullptr;
+  GeometryComposer* composer_ = nullptr;
+
+  static constexpr double kXFactor = 1.0;
+  static constexpr double kYFactor = 1.0;
 
   static const int kOneSecond = 1000;
 };
