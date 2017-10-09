50165d75d969cef57a5b4d93725ce25ca7d6ae71
OrdinaryMind <1.ordinarymind@gmail.com>
2017-10-09 16:57:02 +0300

    Add default method for composing

--- lib_gui/widgets_arrangement/widgets/main_widget_composer.cpp
+++ lib_gui/widgets_arrangement/widgets/main_widget_composer.cpp
@@ -1,1 +1,13 @@
-#include <main_widget_composer.h>
+﻿#include <main_widget_composer.h>
+
+#include <side.h>
+
+MainWidgetComposer::MainWidgetComposer() { SetDefaultComposing(); }
+
+void MainWidgetComposer::ComposeMainFrame(QFrame* frame) {}
+
+void MainWidgetComposer::SetDefaultComposing() {
+  composer_.SetStretchFactor(kXFactor, kYFactor);
+  composer_.SetStretchSide(Side::kRight | Side::kDown);
+  composer_.SetTransformationType(GeometryComposer::kStretch);
+}
--- lib_gui/widgets_arrangement/widgets/main_widget_composer.h
+++ lib_gui/widgets_arrangement/widgets/main_widget_composer.h
@@ -1,4 +1,23 @@
-#ifndef MAIN_WIDGET_COMPOSER_H
+﻿#ifndef MAIN_WIDGET_COMPOSER_H
 #define MAIN_WIDGET_COMPOSER_H
 
-#endif // MAIN_WIDGET_COMPOSER_H
+#include <QFrame>
+
+#include <geometry_composer.h>
+
+class MainWidgetComposer {
+ public:
+  MainWidgetComposer();
+
+  void ComposeMainFrame(QFrame* frame);
+
+ private:
+  void SetDefaultComposing();
+
+  GeometryComposer composer_;
+
+  static constexpr double kXFactor = 1.0;
+  static constexpr double kYFactor = 1.0;
+};
+
+#endif  // MAIN_WIDGET_COMPOSER_H
