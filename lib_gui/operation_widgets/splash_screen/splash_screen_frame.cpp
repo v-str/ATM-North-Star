66a3d99d71198682603bbb5f6bbb3c0ce5584d18
OrdinaryMind <1.ordinarymind@gmail.com>
2017-10-12 13:26:38 +0300

    Move splash screen frame in separate folder

--- lib_gui/operation_widgets/splash_screen/splash_screen_frame.cpp
+++ lib_gui/operation_widgets/splash_screen/splash_screen_frame.cpp
@@ -1,18 +1,73 @@
 ﻿#include <splash_screen_frame.h>
 
+#include <QLabel>
 #include <QList>
+#include <QString>
+#include <QTimer>
 
 #include <side.h>
 #include <splash_screen_geometry.h>
+#include <text_color_swapper.h>
 
 SplashScreenFrame::SplashScreenFrame(QWidget *parent)
     : BaseAtmFrame(parent, kBackButtonDeactivated) {
-  SetInitialSettings();
+  SetSplashScreenSettings();
+  InitializeObjects();
+  TuneLabel();
+  PaintWidgets();
+  SetConnection();
+
+  color_swap_timer_->start(kBlinkTime);
 }
 
-SplashScreenFrame::~SplashScreenFrame() {}
+SplashScreenFrame::~SplashScreenFrame() {
+  delete color_swap_timer_;
+  delete text_color_swapper_;
+}
 
-void SplashScreenFrame::SetInitialSettings() {
+void SplashScreenFrame::SetCompanyName(const QString &company_name) {
+  company_name_label_->setText(company_name);
+}
+
+void SplashScreenFrame::BlinkAtmLabelColor() {
+  text_color_swapper_->SwapColors(atm_label_);
+}
+
+void SplashScreenFrame::resizeEvent(QResizeEvent *) {
+  composer_.ComputeDeltas(width(), height());
+
+  composer_.ComposeCompanyNameLabel(company_name_label_);
+  composer_.ComposeAtmLabel(atm_label_);
+  composer_.ComposeTextLabel(text_label_);
+}
+
+void SplashScreenFrame::SetSplashScreenSettings() {
   SetInitialFrameGeometry(SplashScreenGeometry::SplashScreenFrame());
   SetFrameAnimation(Side::kUp, Side::kDown, kHalfASecond, this);
+}
+
+void SplashScreenFrame::InitializeObjects() {
+  version_label_ = new QLabel(this);
+  company_name_label_ = new QLabel(this);
+  atm_label_ = new QLabel(this);
+  text_label_ = new QLabel(this);
+
+  color_swap_timer_ = new QTimer(atm_label_);
+  text_color_swapper_ = new TextColorSwapper;
+}
+
+void SplashScreenFrame::TuneLabel() {
+  composer_.TuneLabels(version_label_, company_name_label_, text_label_,
+                       atm_label_);
+}
+
+void SplashScreenFrame::PaintWidgets() {
+  QList<QLabel *> label_list = {version_label_, company_name_label_, atm_label_,
+                                text_label_};
+
+  color_designer_.PaintWidgetSet(&label_list);
+}
+
+void SplashScreenFrame::SetConnection() {
+  connect(color_swap_timer_, SIGNAL(timeout()), SLOT(BlinkAtmLabelColor()));
 }
--- lib_gui/operation_widgets/splash_screen/splash_screen_frame.h
+++ lib_gui/operation_widgets/splash_screen/splash_screen_frame.h
@@ -4,6 +4,12 @@
 #include <base_atm_frame.h>
 
 #include <atm_color_designer.h>
+#include <splash_screen_composer.h>
+
+class QLabel;
+class QString;
+class QTimer;
+class TextColorSwapper;
 
 class SplashScreenFrame : public BaseAtmFrame {
   Q_OBJECT
@@ -12,14 +18,38 @@ class SplashScreenFrame : public BaseAtmFrame {
   explicit SplashScreenFrame(QWidget* parent = nullptr);
   ~SplashScreenFrame();
 
- private:
-  void SetInitialSettings();
+  void SetCompanyName(const QString& company_name);
 
-  GeometryComposer composer_;
+ private slots:
+  void BlinkAtmLabelColor();
+
+ protected:
+  void resizeEvent(QResizeEvent*);
+
+ private:
+  void SetSplashScreenSettings();
+  void InitializeObjects();
+  void TuneLabel();
+
+  void PaintWidgets();
+
+  void SetConnection();
+
+  SplashScreenComposer composer_;
+  AtmColorDesigner color_designer_;
+
+  QTimer* color_swap_timer_ = nullptr;
+  TextColorSwapper* text_color_swapper_ = nullptr;
+
+  QLabel* version_label_ = nullptr;
+  QLabel* company_name_label_ = nullptr;
+  QLabel* atm_label_ = nullptr;
+  QLabel* text_label_ = nullptr;
 
   static constexpr double kXFactor = 1.0;
   static constexpr double kYFactor = 1.0;
 
+  static const int kBlinkTime = 350;
   static const int kHalfASecond = 500;
 };
 
