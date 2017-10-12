512f4af0a2620a1905965dad03ca33001727283d
OrdinaryMind <1.ordinarymind@gmail.com>
2017-10-12 23:52:49 +0300

    Add return time to method

--- lib_gui/operation_widgets/main_frame/main_frame_composer.cpp
+++ lib_gui/operation_widgets/main_frame/main_frame_composer.cpp
@@ -7,22 +7,23 @@
 
 MainFrameComposer::MainFrameComposer() { SetDefaultComposing(); }
 
-void MainFrameComposer::SetMainFrameAppearance(QFrame *main_frame,
-                                               QLabel *time_label,
-                                               QLabel *date_label) {
+void MainFrameComposer::SetMainFrameAppearance(QFrame* main_frame,
+                                               QLabel* time_label,
+                                               QLabel* date_label) {
   main_frame->setGeometry(MainFrameGeometry::MainFrame());
   SetTime(time_label);
   SetDate(date_label);
 }
 
-void MainFrameComposer::PaintMainFrame(QFrame *main_frame, QLabel *time_label,
-                                       QLabel *date_label) {
+void MainFrameComposer::PaintMainFrame(QFrame* main_frame,
+                                       QLabel* time_label,
+                                       QLabel* date_label) {
   color_designer_.PaintFrame(main_frame);
   color_designer_.PaintSingleWidget(time_label);
   color_designer_.PaintSingleWidget(date_label);
 }
 
-void MainFrameComposer::ComposeLabels(QLabel *time_label, QLabel *date_label) {
+void MainFrameComposer::ComposeLabels(QLabel* time_label, QLabel* date_label) {
   composer_.ComposeGeometry(MainFrameGeometry::TimeLabel(), time_label);
   composer_.ComposeGeometry(MainFrameGeometry::DateLabel(), date_label);
 }
@@ -31,19 +32,19 @@ void MainFrameComposer::SetDeltaSize(int delta_width, int delta_height) {
   composer_.SetDeltaSize(DeltaSize(delta_width, delta_height));
 }
 
-MainFrameComposer::SetDefaultComposing() {
+void MainFrameComposer::SetDefaultComposing() {
   composer_.SetShiftFactor(kXFactor, kYFactor);
   composer_.SetShiftSide(Side::kRight);
   composer_.SetTransformationType(GeometryComposer::kShift);
 }
 
-void MainFrameComposer::SetTime(QLabel *time_label) {
+void MainFrameComposer::SetTime(QLabel* time_label) {
   time_label->setGeometry(MainFrameGeometry::TimeLabel());
   time_label->setFont(WidgetFont::SetFont(12));
   time_label->setAlignment(Qt::AlignRight);
 }
 
-void MainFrameComposer::SetDate(QLabel *date_label) {
+void MainFrameComposer::SetDate(QLabel* date_label) {
   date_label->setGeometry(MainFrameGeometry::DateLabel());
   date_label->setFont(WidgetFont::SetFont(12));
   date_label->setAlignment(Qt::AlignRight);
--- lib_gui/operation_widgets/main_frame/main_frame_composer.h
+++ lib_gui/operation_widgets/main_frame/main_frame_composer.h
@@ -11,9 +11,11 @@ class MainFrameComposer {
  public:
   MainFrameComposer();
 
-  void SetMainFrameAppearance(QFrame* main_frame, QLabel* time_label,
+  void SetMainFrameAppearance(QFrame* main_frame,
+                              QLabel* time_label,
                               QLabel* date_label);
-  void PaintMainFrame(QFrame* main_frame, QLabel* time_label,
+  void PaintMainFrame(QFrame* main_frame,
+                      QLabel* time_label,
                       QLabel* date_label);
 
   void ComposeLabels(QLabel* time_label, QLabel* date_label);
@@ -21,7 +23,7 @@ class MainFrameComposer {
   void SetDeltaSize(int delta_width, int delta_height);
 
  private:
-  SetDefaultComposing();
+  void SetDefaultComposing();
 
   void SetTime(QLabel* time_label);
   void SetDate(QLabel* date_label);
