bde0132c41022768e018a8c0d65d4af646fb5a81
OrdinaryMind <1.ordinarymind@gmail.com>
2017-10-14 20:09:34 +0300

    Change description frame geometry

--- lib_gui/widgets_arrangement/common/widgets_geometries/registration_menu_geometry.cpp
+++ lib_gui/widgets_arrangement/common/widgets_geometries/registration_menu_geometry.cpp
@@ -1,7 +1,7 @@
 ﻿#include <registration_menu_geometry.h>
 
 QRect RegistrationMenuGeometry::kRegistrationFrame = {5, 30, 580, 355};
-QRect RegistrationMenuGeometry::kDescriptionMenu = {45, 30, 395, 225};
+QRect RegistrationMenuGeometry::kDescriptionMenu = {5, 5, 570, 310};
 QRect RegistrationMenuGeometry::kBackButton = {5, 320, 50, 30};
 
 QRect RegistrationMenuGeometry::RegistrationFrame() {
