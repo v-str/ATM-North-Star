d0ae839cb94a332118673044696bfdfda48ce12b
OrdinaryMind <1.ordinarymind@gmail.com>
2017-10-14 11:54:05 +0300

    Fill basic composer functionality

--- lib_gui/operation_widgets/registration_menu/registration_composer.cpp
+++ lib_gui/operation_widgets/registration_menu/registration_composer.cpp
@@ -1,1 +1,21 @@
 ﻿#include <registration_composer.h>
+
+#include <QWidget>
+
+#include <registration_menu_geometry.h>
+#include <side.h>
+
+RegistrationComposer::RegistrationComposer() { SetDefaultComposingSettings(); }
+
+void RegistrationComposer::ComposeGeometry(const DeltaSize& delta_size,
+                                           QWidget* widget_for_compose) {
+  composer_.SetDeltaSize(delta_size);
+  composer_.ComposeGeometry(RegistrationMenuGeometry::RegistrationFrame(),
+                            widget_for_compose);
+}
+
+void RegistrationComposer::SetDefaultComposingSettings() {
+  composer_.SetStretchFactor(kXFactor, kYFactor);
+  composer_.SetStretchSide(Side::kRight | Side::kDown);
+  composer_.SetTransformationType(GeometryComposer::kStretch);
+}
--- lib_gui/operation_widgets/registration_menu/registration_composer.h
+++ lib_gui/operation_widgets/registration_menu/registration_composer.h
@@ -1,8 +1,25 @@
 ﻿#ifndef REGISTRATION_COMPOSER_H
 #define REGISTRATION_COMPOSER_H
 
+#include <delta_size.h>
+#include <geometry_composer.h>
+
+class QWidget;
+
 class RegistrationComposer {
  public:
+  RegistrationComposer();
+
+  void ComposeGeometry(const DeltaSize& delta_size,
+                       QWidget* widget_for_compose);
+
+ private:
+  void SetDefaultComposingSettings();
+
+  GeometryComposer composer_;
+
+  static constexpr double kXFactor = 1.0;
+  static constexpr double kYFactor = 1.0;
 };
 
 #endif  // REGISTRATION_COMPOSER_H
