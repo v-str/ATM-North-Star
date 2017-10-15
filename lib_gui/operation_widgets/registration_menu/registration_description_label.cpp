ce87275dd213c8e99b369a7a0a51d7c66f6bf578
OrdinaryMind <1.ordinarymind@gmail.com>
2017-10-15 18:06:04 +0300

    Add class for registration description label

---
+++ lib_gui/operation_widgets/registration_menu/registration_description_label.cpp
@@ -0,0 +1,8 @@
+﻿#include <registration_description_label.h>
+
+#include <QWidget>
+
+RegistrationDescriptionLabel::RegistrationDescriptionLabel(QWidget* parent)
+    : QLabel(parent) {}
+
+RegistrationDescriptionLabel::~RegistrationDescriptionLabel() {}
---
+++ lib_gui/operation_widgets/registration_menu/registration_description_label.h
@@ -0,0 +1,15 @@
+﻿#ifndef REGISTRATION_DESCRIPTION_LABEL_H
+#define REGISTRATION_DESCRIPTION_LABEL_H
+
+#include <QLabel>
+
+class QWidget;
+
+class RegistrationDescriptionLabel : public QLabel {
+  Q_OBJECT
+ public:
+  RegistrationDescriptionLabel(QWidget* parent = nullptr);
+  ~RegistrationDescriptionLabel();
+};
+
+#endif  // REGISTRATION_DESCRIPTION_LABEL_H
