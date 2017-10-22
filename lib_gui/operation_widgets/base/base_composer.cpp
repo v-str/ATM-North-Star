19c96818864da42c09c43d4c8ac06e7e0e5dcd2c
OrdinaryMind <1.ordinarymind@gmail.com>
2017-10-22 11:28:49 +0300

    Set initial back button composer settings

--- lib_gui/operation_widgets/base/base_composer.cpp
+++ lib_gui/operation_widgets/base/base_composer.cpp
@@ -1,3 +1,14 @@
 ﻿#include <base_composer.h>
 
-void BaseComposer::SetBackButtonSettings() {}
+#include <back_button_property.h>
+#include <side.h>
+
+void BaseComposer::SetBackButtonSettings() {
+  back_button_composer_.SetShiftFactor(BackButtonProperty::XShift(),
+                                       BackButtonProperty::YShift());
+  back_button_composer_.SetShiftSide(Side::kRight | Side::kDown);
+  back_button_composer_.SetStretchFactor(BackButtonProperty::XStretch(),
+                                         BackButtonProperty::YStretch());
+  back_button_composer_.SetStretchSide(Side::kUp | Side::kRight);
+  back_button_composer_.SetTransformationType(GeometryComposer::kScale);
+}
