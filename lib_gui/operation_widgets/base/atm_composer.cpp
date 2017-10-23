ffa7ed5afdc76dc869ef630e4d6ce0fab11dc0f3
OrdinaryMind <1.ordinarymind@gmail.com>
2017-10-23 15:55:57 +0300

    Add methods for composing

--- lib_gui/operation_widgets/base/atm_composer.cpp
+++ lib_gui/operation_widgets/base/atm_composer.cpp
@@ -4,6 +4,9 @@
 #include <side.h>
 
 BackButtonComposer AtmComposer::kBackButtonComposer;
+GeometryComposer AtmComposer::kShiftComposer;
+GeometryComposer AtmComposer::kStretchComposer;
+GeometryComposer AtmComposer::kScaleComposer;
 DeltaSize AtmComposer::kDeltaSize;
 
 void AtmComposer::SetDeltaSize(const DeltaSize& delta_size) {
@@ -12,6 +15,49 @@ void AtmComposer::SetDeltaSize(const DeltaSize& delta_size) {
 
 void AtmComposer::ComposeBackButton(AtmButton* back_button) {
   kBackButtonComposer.ComposeBackButton(kDeltaSize, back_button);
+}
+
+void AtmComposer::ShiftWidget(const QRect& initial_geometry,
+                              unsigned int shift_side,
+                              double x_shift_factor,
+                              double y_shift_factor,
+                              QWidget* widget) {
+  kShiftComposer.SetDeltaSize(kDeltaSize);
+  kShiftComposer.SetShiftSide(shift_side);
+  kShiftComposer.SetShiftFactor(x_shift_factor, y_shift_factor);
+  kShiftComposer.SetTransformationType(GeometryComposer::kShift);
+  kShiftComposer.ComposeGeometry(initial_geometry, widget);
+}
+
+void AtmComposer::StretchWidget(const QRect& initial_geometry,
+                                unsigned int stretch_side,
+                                double x_stretch_factor,
+                                double y_stretch_factor,
+                                QWidget* widget) {
+  kStretchComposer.SetDeltaSize(kDeltaSize);
+  kStretchComposer.SetStretchSide(stretch_side);
+  kStretchComposer.SetStretchFactor(x_stretch_factor, y_stretch_factor);
+  kStretchComposer.SetTransformationType(GeometryComposer::kStretch);
+  kStretchComposer.ComposeGeometry(initial_geometry, widget);
+}
+
+void AtmComposer::SetScaleWidgetSide(unsigned int shift_side,
+                                     unsigned int stretch_side) {
+  kScaleComposer.SetShiftSide(shift_side);
+  kScaleComposer.SetStretchSide(stretch_side);
+}
+
+void AtmComposer::ScaleWidget(const QRect& initial_geometry,
+                              double x_shift_factor,
+                              double y_shift_factor,
+                              double x_stretch_factor,
+                              double y_stretch_factor,
+                              QWidget* widget) {
+  kScaleComposer.SetDeltaSize(kDeltaSize);
+  kScaleComposer.SetShiftFactor(x_shift_factor, y_shift_factor);
+  kScaleComposer.SetStretchFactor(x_stretch_factor, y_stretch_factor);
+  kScaleComposer.SetTransformationType(GeometryComposer::kScale);
+  kScaleComposer.ComposeGeometry(initial_geometry, widget);
 }
 
 DeltaSize AtmComposer::GetDeltaSize() { return kDeltaSize; }
--- lib_gui/operation_widgets/base/atm_composer.h
+++ lib_gui/operation_widgets/base/atm_composer.h
@@ -14,7 +14,25 @@ class AtmComposer {
  public:
   static void SetDeltaSize(const DeltaSize& delta_size);
   static void ComposeBackButton(AtmButton* back_button);
-  static void ComposeWidget(const QRect& widget_geometry, QWidget* widget);
+  static void ShiftWidget(const QRect& initial_geometry,
+                          unsigned int shift_side,
+                          double x_shift_factor,
+                          double y_shift_factor,
+                          QWidget* widget);
+  static void StretchWidget(const QRect& initial_geometry,
+                            unsigned int stretch_side,
+                            double x_stretch_factor,
+                            double y_stretch_factor,
+                            QWidget* widget);
+
+  static void SetScaleWidgetSide(unsigned int shift_side,
+                                 unsigned int stretch_side);
+  static void ScaleWidget(const QRect& initial_geometry,
+                          double x_shift_factor,
+                          double y_shift_factor,
+                          double x_stretch_factor,
+                          double y_stretch_factor,
+                          QWidget* widget);
 
   static DeltaSize GetDeltaSize();
 
@@ -22,6 +40,10 @@ class AtmComposer {
   static void SetBackButtonSettings();
 
   static BackButtonComposer kBackButtonComposer;
+  static GeometryComposer kShiftComposer;
+  static GeometryComposer kStretchComposer;
+  static GeometryComposer kScaleComposer;
+
   static DeltaSize kDeltaSize;
 };
 
