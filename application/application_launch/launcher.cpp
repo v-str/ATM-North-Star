f31f615f36f26adf65cbdf9b633841042545f840
OrdinaryMind <1.ordinarymind@gmail.com>
2017-11-03 10:09:24 +0300

    Change configuration file description

--- application/application_launch/launcher.cpp
+++ application/application_launch/launcher.cpp
@@ -26,13 +26,15 @@ void Launcher::LaunchGuiMode(int argc, char* argv[]) {
 
 void Launcher::DisplayErrorReport() const {
   std::cout
-      << "Error while reading configuration file \"app_config.txt\"\n"
-         "Configuration file was not created or contain incorrect data...\n\n\n"
-         "For successful program launching you must to create a "
-         "\"app_config.txt\" file\n"
-         "and put it near the executed file.\n"
+      << "Error while reading configuration file.\n"
+         "Configuration file was not created or contain incorrect data...\n\n"
+         "For successful program launching you must to create a\n"
+         "configuration file.\n\n"
+         "For Linux: app_config.txt\n"
+         "For Windows: app_config\n\n"
+         "Put it near the executed file.\n"
          "In configuration file you must write which mode of app you want to "
          "use.\n"
          "If you want to run app in console mode, write \"Mode: cli\"\n"
-         "Or if you want to run app in graphical mode, write \"Mode: gui\"\n";
+         "Or if you want to run app in graphical mode, write \"Mode: gui\"\n\n";
 }
