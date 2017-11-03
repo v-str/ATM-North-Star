94b1d77e80875b86d30e483841bf64ba3c6bfde3
OrdinaryMind <1.ordinarymind@gmail.com>
2017-11-03 15:46:58 +0300

    Add initial checkbox settings

--- lib_gui/operation_widgets/registration_menu/familiarized_checkbox.cpp
+++ lib_gui/operation_widgets/registration_menu/familiarized_checkbox.cpp
@@ -1,2 +1,12 @@
 ﻿#include <familiarized_checkbox.h>
 
+#include <registration_menu_geometry.h>
+#include <widget_font.h>
+
+FamiliarizedCheckbox::FamiliarizedCheckbox(QWidget *parent)
+    : QCheckBox("I'm familiarized", parent) {
+  setGeometry(RegistrationMenuGeometry::FamiliarizedCheckbox());
+  setFont(WidgetFont::SetFont(14));
+}
+
+FamiliarizedCheckbox::~FamiliarizedCheckbox() {}
--- lib_gui/operation_widgets/registration_menu/familiarized_checkbox.h
+++ lib_gui/operation_widgets/registration_menu/familiarized_checkbox.h
@@ -2,9 +2,12 @@
 #define FAMILIARIZED_CHECKBOX_H
 
 #include <QCheckBox>
+#include <QString>
 
 class FamiliarizedCheckbox : public QCheckBox {
  public:
+  FamiliarizedCheckbox(QWidget* parent = nullptr);
+  ~FamiliarizedCheckbox();
 };
 
 #endif  // FAMILIARIZED_CHECKBOX_H
