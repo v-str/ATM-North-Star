e17a9c2775e20494b44e0a30f00682d81d734767
OrdinaryMind <1.ordinarymind@gmail.com>
2017-11-07 18:34:09 +0300

    Add class for storing inital widget parameters For font controlling

---
+++ lib_gui/widgets_arrangement/common/modificators/primitive/font_size_controller/iwcs.h
@@ -0,0 +1,16 @@
+﻿/* IWCS - Initial widget control sizes
+Special class for storing initial widget parameters
+iww_ - inital widget width
+iwh_ - initial widget height
+ifh_ - initial font height
+ifps_ - initial font pixel size
+ifch_ - initial font height coefficient
+ifcw_ - initial font width coefficient
+*/
+
+#ifndef IWCS_H
+#define IWCS_h
+
+class IWCS {};
+
+#endif  // IWCS_H
