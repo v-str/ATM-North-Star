5a8a2f5e5fce12d756d7290bb7e9a491c319f3d0
OrdinaryMind <1.ordinarymind@gmail.com>
2017-11-07 18:43:36 +0300

    Add setters and getters for class

--- lib_gui/widgets_arrangement/common/modificators/primitive/font_size_controller/iwcs.cpp
+++ lib_gui/widgets_arrangement/common/modificators/primitive/font_size_controller/iwcs.cpp
@@ -1,1 +1,25 @@
-#include <iwcs.h>
+﻿#include <iwcs.h>
+
+void IWCS::SetIww(int iww) { iww_ = iww; }
+
+void IWCS::SetIwh(int iwh) { iwh_ = iwh; }
+
+void IWCS::SetIfh(int ifh) { ifh_ = ifh; }
+
+void IWCS::SetIfps(int ifps) { ifps_ = ifps; }
+
+void IWCS::SetIfch(int ifch) { ifch_ = ifch; }
+
+void IWCS::SetIfcw(int ifcw) { ifcw_ = ifcw; }
+
+int IWCS::GetIww() const { return iww_; }
+
+int IWCS::GetIwh() const { return iwh_; }
+
+int IWCS::GetIfh() const { return ifh_; }
+
+int IWCS::GetIfps() const { return ifps_; }
+
+int IWCS::GetIfch() const { return ifch_; }
+
+int IWCS::GetIfcw() const { return ifcw_; }
--- lib_gui/widgets_arrangement/common/modificators/primitive/font_size_controller/iwcs.h
+++ lib_gui/widgets_arrangement/common/modificators/primitive/font_size_controller/iwcs.h
@@ -11,6 +11,29 @@ ifcw_ - initial font width coefficient
 #ifndef IWCS_H
 #define IWCS_h
 
-class IWCS {};
+class IWCS {
+ public:
+  void SetIww(int iww);
+  void SetIwh(int iwh);
+  void SetIfh(int ifh);
+  void SetIfps(int ifps);
+  void SetIfch(int ifch);
+  void SetIfcw(int ifcw);
+
+  int GetIww() const;
+  int GetIwh() const;
+  int GetIfh() const;
+  int GetIfps() const;
+  int GetIfch() const;
+  int GetIfcw() const;
+
+ private:
+  int iww_ = 0;
+  int iwh_ = 0;
+  int ifh_ = 0;
+  int ifps_ = 0;
+  int ifch_ = 0;
+  int ifcw_ = 0;
+};
 
 #endif  // IWCS_H
